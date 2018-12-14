fd2bf82ff95d28d6dae18c468a7974cfe73269ae
John Pignata <john@pignata.com>
2018-12-14 13:56:45 -0500

    Day 14, still brute force, but faster

--- 2018/14/solve.rb
+++ 2018/14/solve.rb
@@ -1,15 +1,25 @@
 input = 556061
-
+sequence = input.digits.reverse
 answer_size = 10
 scores = [3, 7]
 elf1 = 0
 elf2 = 1
+count = scores.size
+index = 0
 
-1.upto((input + answer_size) * 100) do
-  (scores[elf1] + scores[elf2]).digits.reverse.each { |d| scores << d }
+1.step do |i|
+  (scores[elf1] + scores[elf2]).digits.reverse.each do |d|
+    scores << d
+    count += 1
+    index = sequence[index] == d ? index + 1 : 0
+
+    if index == sequence.size
+      puts count - sequence.size
+      puts scores[input...input+answer_size].map(&:to_s).join
+      exit
+    end
+  end
+
   elf1 = (elf1 + 1 + scores[elf1]) % scores.size
   elf2 = (elf2 + 1 + scores[elf2]) % scores.size
 end
-
-puts scores[input...input+answer_size].map(&:to_s).join
-puts scores.map(&:to_s).join.index(input.digits.reverse.map(&:to_s).join)

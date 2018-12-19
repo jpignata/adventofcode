65627b6f68073b5e588fc23524310a306ee8831a
John Pignata <john@pignata.com>
2018-12-19 16:52:10 -0500

    Day 19

---
+++ 2018/19/solve.rb
@@ -0,0 +1,112 @@
+Program = Struct.new(:instructions, :ip)
+
+class Device
+  attr_accessor :ip
+  attr_reader :registers
+
+  def initialize(input = Array.new(6, 0))
+    @registers = input.dup
+  end
+
+  def run(program)
+    ip = registers[program.ip]
+    instruction = program.instructions.first
+
+    loop do
+      break if ip >= program.instructions.length
+
+      instruction = program.instructions[ip]
+      registers[program.ip] = ip
+      start = registers.dup
+      send(instruction[0], instruction[1..-1])
+      ip = registers[program.ip] + 1
+    end
+  end
+
+  private
+
+  def addr((a, b, c))
+    registers[c] = registers[a] + registers[b]
+  end
+
+  def addi((a, b, c))
+    registers[c] = registers[a] + b
+  end
+
+  def mulr((a, b, c))
+    registers[c] = registers[a] * registers[b]
+  end
+
+  def muli((a, b, c))
+    registers[c] = registers[a] * b
+  end
+
+  def banr((a, b, c))
+    registers[c] = registers[a] & registers[b]
+  end
+
+  def bani((a, b, c))
+    registers[c] = registers[a] & b
+  end
+
+  def borr((a, b, c))
+    registers[c] = registers[a] | registers[b]
+  end
+
+  def bori((a, b, c))
+    registers[c] = registers[a] | b
+  end
+
+  def setr((a, b, c))
+    registers[c] = registers[a] 
+  end
+
+  def seti((a, b, c))
+    registers[c] = a
+  end
+
+  def gtir((a, b, c))
+    registers[c] = a > registers[b] ? 1 : 0
+  end
+
+  def gtri((a, b, c))
+    registers[c] = registers[a] > b ? 1 : 0
+  end
+
+  def gtrr((a, b, c))
+    registers[c] = registers[a] > registers[b] ? 1 : 0
+  end
+
+  def eqir((a, b, c))
+    registers[c] = a == registers[b] ? 1 : 0
+  end
+
+  def eqri((a, b, c))
+    registers[c] = registers[a] == b ? 1 : 0
+  end
+
+  def eqrr((a, b, c))
+    registers[c] = registers[a] == registers[b] ? 1 : 0
+  end
+end
+
+instructions = []
+ip = 0
+
+ARGF.readlines.each do |line|
+  if line.start_with?("#ip")
+    ip = line.scan(/\d+/).first.to_i
+  else
+    instruction = line.chomp.split
+    instructions << [instruction[0], *instruction[1..-1].map(&:to_i)]
+  end
+end
+
+device = Device.new
+program = Program.new(instructions, ip)
+device.run(program)
+puts device.registers[0]
+
+[994, 10551394].each do |num|
+  puts (2..num).select { |i| num % i == 0 }.sum + 1
+end

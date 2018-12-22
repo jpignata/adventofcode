ef5522179c8ff8b2f232d35cb024610066bf27d2
John Pignata <john@pignata.com>
2018-12-22 18:55:52 -0500

    Day 22, Part 2

--- 2018/22/solve.rb
+++ 2018/22/solve.rb
@@ -1,10 +1,20 @@
-Region = Struct.new(:index, :erosion_level, :type)
+require 'set'
+
+Region = Struct.new(:x, :y, :index, :erosion_level, :type)
+Edge = Struct.new(:from, :to, :weight, :start_equipment, :end_equipment)
+Visit = Struct.new(:region, :equipment, :minutes)
 
 TYPES = [:rocky, :wet, :narrow]
 
+EQUIPMENT = {
+  rocky: [:climbing_gear, :torch],
+  wet: [:climbing_gear, :neither],
+  narrow: [:torch, :neither]
+}
+
 depth, target = ARGV[0].to_i, ARGV[1].split(",").map(&:to_i)
 
-grid = Array.new(target[1] + 1) { Array.new(target[0] + 1) }
+grid = Array.new(target[1] * 2) { Array.new(target[0] * 2) }
 
 grid.each.with_index do |row, y|
   row.each.with_index do |cell, x|
@@ -23,12 +33,60 @@ grid.each.with_index do |row, y|
     erosion_level = (index + depth) % 20183
     type = TYPES[erosion_level % 3]
 
-    grid[y][x] = Region.new( index, erosion_level, type)
+    grid[y][x] = Region.new(x, y, index, erosion_level, type)
   end
 end
 
-risk_level = grid.inject(0) do |acc, row|
-  acc += row.sum { |region| TYPES.index(region.type) }
+risk_level = grid[0..target[1]].inject(0) do |acc, row|
+  acc += row[0..target[0]].sum { |region| TYPES.index(region.type) }
 end
 
 puts risk_level
+
+edges = grid.map.with_index do |row, y|
+  row.map.with_index do |region, x|
+    adjacent = [[-1, 0], [1, 0], [0, -1], [0, 1]].map { |xdiff, ydiff|
+      coords = [x + xdiff, y + ydiff]
+
+      next if coords.any?(&:negative?)
+      next if coords[0] > grid[0].length - 1
+      next if coords[1] > grid.length - 1
+
+      grid[coords[1]][coords[0]]
+    }.compact
+
+    adjacent.map { |other|
+      EQUIPMENT[region.type].product(EQUIPMENT[other.type]).map do |eq1, eq2|
+        weight = 1
+        weight += 7 if eq1 != eq2
+        Edge.new(region, other, weight, eq1, eq2)
+      end
+    }.flatten
+  end
+end
+
+queue = []
+routes = Hash.new(Float::INFINITY)
+
+queue << Visit.new(grid[0][0], :torch, 0)
+
+while queue.any?
+  visit = queue.sort_by!(&:minutes).shift
+
+  if visit.region == grid[target[1]][target[0]] && visit.equipment == :torch
+    puts visit.minutes
+    break
+  end
+
+  edges[visit.region.y][visit.region.x].each do |edge|
+    next unless edge.start_equipment == visit.equipment
+
+    distance = visit.minutes + edge.weight
+
+    if routes[edge] > distance
+      queue << Visit.new(edge.to, edge.end_equipment, distance)
+      routes[edge] = distance
+    end
+  end
+end
+

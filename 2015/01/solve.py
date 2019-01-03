a1bacaef75f602e9fffd366edc40c104cb5b140d
John Pignata <john@pignata.com>
2019-01-02 22:31:20 -0500

    2015/01

---
+++ 2015/01/solve.py
@@ -0,0 +1,13 @@
+import sys
+
+instructions = sys.stdin.readline()
+floor = 0
+
+print('Part 1:', instructions.count('(') - instructions.count(')'))
+
+for i, char in enumerate(instructions):
+    floor += (-1, 1)[char == '(']
+
+    if floor == -1:
+        print('Part 2:', i + 1)
+        break

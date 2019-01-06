52e89f2852555c4f28290df8ba0a478a9337ffc7
John Pignata <john@pignata.com>
2019-01-06 08:27:31 -0500

    Tweaks for PEP8
    I ignored E731 because I don't wanna.

--- 2015/03/solve.py
+++ 2015/03/solve.py
@@ -6,6 +6,7 @@ from itertools import cycle
 directions = sys.stdin.readline().strip()
 moves = {'>': (0, 1), '<': (0, -1), '^': (1, 0), 'v': (-1, 0)}
 
+
 def deliver(number_of_santas=1):
     start = (0, 0)
     houses = Counter({start: number_of_santas})
@@ -19,6 +20,6 @@ def deliver(number_of_santas=1):
 
     return len(houses)
 
+
 print('Part 1:', deliver())
 print('Part 2:', deliver(2))
-

b0919b5c10720dc7c26cce5195a5827b40b3c8be
John Pignata <john@pignata.com>
2019-01-07 19:31:00 -0500

    2015/08

---
+++ 2015/08/solve.py
@@ -0,0 +1,34 @@
+import sys
+import re
+from collections import deque
+
+
+def escape(line):
+    original = deque(line)
+    escaped = []
+
+    while len(original) != 0:
+        char = original.popleft()
+
+        if char == '"':
+            escaped.append('\\"')
+        elif char == "\\" and original[0] == '"':
+            original.popleft()
+            escaped.append('\\\\\\"')
+        elif char == '\\' and original[0] == '\\':
+            original.popleft()
+            escaped.append('\\\\\\\\')
+        elif char == '\\' and original[0] == 'x':
+            escaped.append('\\\\')
+        else:
+            escaped.append(char)
+
+    return '"' + ''.join(escaped) + '"'
+
+
+lines = [lines.strip() for lines in sys.stdin.readlines()]
+unescaped = [bytes(l[1:-1], 'utf-8').decode('unicode_escape') for l in lines]
+escaped = [escape(line) for line in lines]
+
+print('Part 1:', sum([len(l) for l in lines]) - sum([len(s) for s in unescaped]))
+print('Part 2:', sum([len(l) for l in escaped]) - sum([len(l) for l in lines]))

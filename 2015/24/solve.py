46191dd7715c9c57b8f0dc8a3c202d119592b457
John Pignata <john@pignata.com>
2019-01-19 21:24:17 -0500

    2015/24

---
+++ 2015/24/solve.py
@@ -0,0 +1,17 @@
+import sys
+import operator
+from itertools import combinations
+from functools import reduce
+
+
+def find_smallest(nums, target):
+    for l in range(len(nums)):
+        for c in combinations(nums, l):
+            if sum(c) == target:
+                return reduce(operator.mul, c)
+
+
+weights = [int(weight.strip()) for weight in sys.stdin.readlines()]
+
+print('Part 1:', find_smallest(weights, sum(weights) / 3))
+print('Part 2:', find_smallest(weights, sum(weights) / 4))

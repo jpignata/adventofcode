564a02848cbdf418a300c1129bf527e8cb52e8e5
John Pignata <john@pignata.com>
2019-01-25 20:45:33 -0500

    2016/09

---
+++ 2016/09/solve.py
@@ -0,0 +1,36 @@
+import sys
+
+
+def size_of(data, v2=False):
+    count = 0
+    i = 0
+
+    while i < len(data):
+        if data[i] == '(':
+            marker = ''
+            i += 1
+
+            while data[i] != ')':
+                marker += data[i]
+                i += 1
+
+            num_chars, reps = map(int, marker.split('x'))
+
+            if v2:
+                count += size_of(data[i+1:i+num_chars+1], v2) * reps
+            else:
+                count += num_chars * reps
+
+            i += num_chars
+        else:
+            count += 1
+
+        i += 1
+
+    return count
+
+
+data = sys.stdin.readline().strip()
+
+print('Part 1:', size_of(data))
+print('Part 2:', size_of(data, True))

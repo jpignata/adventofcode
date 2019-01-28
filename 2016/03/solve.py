fe3d42dfef3fe5e8c0ff95d22e32d17470515f73
John Pignata <john@pignata.com>
2019-01-28 09:27:56 -0500

    2016/03, use numpy

--- 2016/03/solve.py
+++ 2016/03/solve.py
@@ -1,26 +1,15 @@
 import sys
-import re
+import numpy as np
 
 
 def count(triangles):
-    count = 0
-    for a, b, c in triangles:
-        if a + b > c and a + c > b and b + c > a:
-            count += 1
-    return count
+    return sum(1 for a, b, c in triangles
+               if a + b > c and a + c > b and b + c > a)
 
 
-triangles = [list(map(int, re.split(r'\s+', line.strip())))
-             for line in sys.stdin.readlines()]
-rows = count(triangles)
-cols = 0
+triangles = np.loadtxt(sys.stdin.readlines())
+transposed = triangles.transpose().flatten()
+transposed.shape = int(len(transposed) / 3), 3
 
-for i in range(0, len(triangles), 3):
-    transposed = ([], [], [])
-    for j in range(0, 3):
-        for k in range(0, 3):
-            transposed[j].append(triangles[i+k][j])
-    cols += count(transposed)
-
-print('Part 1:', rows)
-print('Part 2:', cols)
+print('Part 1:', count(triangles))
+print('Part 2:', count(transposed))

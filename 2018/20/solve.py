80f56f1483186ae76f0deb5df2cf29fb91d7b8f9
John Pignata <john@pignata.com>
2019-01-28 23:01:37 -0500

    2018/20, in Python

---
+++ 2018/20/solve.py
@@ -0,0 +1,24 @@
+import sys
+import operator
+from collections import defaultdict
+
+current = (0, 0)
+directions = {'N': (0, -1), 'S': (0, 1), 'E': (1, 0), 'W': (-1, 0)}
+rooms = defaultdict(lambda: float('inf'))
+rooms[current] = 0
+context = []
+
+for char in sys.stdin.readline().strip():
+    if char == '(':
+        context.append(current)
+    elif char == '|':
+        current = context[-1]
+    elif char == ')':
+        current = context.pop()
+    elif char in directions:
+        cost = rooms[current]
+        current = tuple(map(operator.add, current, directions[char]))
+        rooms[current] = min(rooms[current], cost + 1)
+
+print('Part 1:', max(rooms.values()))
+print('Part 2:', sum(1 for cost in rooms.values() if cost >= 1000) )

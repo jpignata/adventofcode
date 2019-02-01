6d6f8962533f1507ad49e146290f6d8144609595
John Pignata <john@pignata.com>
2019-02-01 12:20:50 -0500

    2016/15, tidy up

--- 2016/15/solve.py
+++ 2016/15/solve.py
@@ -5,12 +5,15 @@ def find(discs):
     disc = 0
 
     for second in count(1):
-        if disc == len(discs):
-            return second - len(discs) - 1
-        elif (discs[disc][1] + second) % discs[disc][0] == 0:
+        positions, start = discs[disc]
+
+        if (start + second) % positions == 0:
             disc += 1
         else:
             disc = 0
+
+        if disc == len(discs):
+            return second - len(discs)
 
 
 discs = [(17, 1), (7, 0), (19, 2), (5, 0), (3, 0), (13, 5)]

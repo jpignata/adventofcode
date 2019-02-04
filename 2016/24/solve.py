ccd8b352d162a2c05a0f2ae16f9092bdea5e3d9c
John Pignata <john@pignata.com>
2019-02-04 07:25:17 -0500

    2016/24, tidy up

--- 2016/24/solve.py
+++ 2016/24/solve.py
@@ -5,51 +5,41 @@ from collections import defaultdict, deque
 
 def fewest_steps(grid, *, return_home=False):
     distances = defaultdict(dict)
-
-    for start_location, end_location in permutations(find_locations(grid), 2):
-        if end_location not in distances[start_location]:
-            cost = bfs(grid, start_location, end_location)
-            distances[start_location][end_location] = cost
-            distances[end_location][start_location] = cost
-
-    min_distance = float('inf')
-    first = min(distances['0'])
-    distance = distances['0'][first]
-
-    locations = find_locations(grid)
-    locations.remove('0')
-    locations.remove(first)
-
-    for path in permutations(locations, len(locations)):
-        new_distance = distance
-        next = first
-
-        for loc in path:
-            new_distance += distances[next][loc]
-            next = loc
-
-        if return_home:
-            new_distance += distances[next]['0']
-
-        min_distance = min(min_distance, new_distance)
-
-    return min_distance
-
-
-def find_locations(grid):
     locations = []
+    vertices = dict()
 
     for y, row in enumerate(grid):
         for x, char in enumerate(row):
             if char.isdigit():
                 locations.append(char)
+                vertices[char] = (x, y)
 
-    return locations
+    for start, target in permutations(locations, 2):
+        if target not in distances[start]:
+            cost = bfs(grid, vertices[start], vertices[target])
+            distances[start][target] = cost
+            distances[target][start] = cost
+
+    min_distance = float('inf')
+    locations.remove('0')
+
+    for path in permutations(locations, len(locations)):
+        distance = 0
+        prev = '0'
+
+        for location in path:
+            distance += distances[prev][location]
+            prev = location
+
+        if return_home:
+            distance += distances[prev]['0']
+
+        min_distance = min(min_distance, distance)
+
+    return min_distance
 
 
-def bfs(grid, start_location, end_location):
-    start = find_location(grid, start_location)
-    target = find_location(grid, end_location)
+def bfs(grid, start, target):
     costs = {start: 0}
     q = deque([start])
 
@@ -67,13 +57,6 @@ def bfs(grid, start_location, end_location):
                 if (nx, ny) not in costs or costs[(nx, ny)] > new_cost:
                     q.append((nx, ny))
                     costs[(nx, ny)] = new_cost
-
-
-def find_location(grid, location):
-    for y, row in enumerate(grid):
-        for x, char in enumerate(row):
-            if char == location:
-                return (x, y)
 
 
 grid = [list(line.strip()) for line in sys.stdin.readlines()]

b29bf7a25bb7efbbba6da41b80b5f0dca342b093
John Pignata <john@pignata.com>
2019-02-09 17:53:26 -0500

    2017/05

---
+++ 2017/05/solve.py
@@ -0,0 +1,25 @@
+import sys
+from itertools import count
+
+
+def escape(instructions, *, strange_mode=False):
+    ip = 0
+
+    for i in count(0):
+        if ip < 0 or ip >= len(instructions):
+            return i
+
+        next_ip = ip + instructions[ip]
+
+        if strange_mode and instructions[ip] >= 3:
+            instructions[ip] -= 1
+        else:
+            instructions[ip] += 1
+
+        ip = next_ip
+
+
+instructions = [int(line) for line in sys.stdin.readlines()]
+
+print('Part 1', escape(instructions.copy()))
+print('Part 2', escape(instructions.copy(), strange_mode=True))

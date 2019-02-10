9b9639650e1d75f2bd4f0dd0dcdbeadb27e51ce8
John Pignata <john@pignata.com>
2019-02-10 18:41:51 -0500

    2017/09

---
+++ 2017/09/solve.py
@@ -0,0 +1,24 @@
+import sys
+
+chars = iter(sys.stdin.readline())
+depth = 0
+score = 0
+garbage = 0
+
+for char in chars:
+    if char == '{':
+        depth += 1
+    elif char == '}':
+        score += depth
+        depth -= 1
+    elif char == '<':
+        while char != '>':
+            char = next(chars)
+
+            if char == '!':
+                next(chars)
+            elif char != '>':
+                garbage += 1
+
+print('Part 1:', score)
+print('Part 2:', garbage)

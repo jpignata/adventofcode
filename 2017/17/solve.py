2b3e65d4cdeb9550f5af13c4b5e736889955c977
John Pignata <john@pignata.com>
2019-02-16 20:56:27 -0500

    2017/17

---
+++ 2017/17/solve.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+steps = 343
+buffer = deque([0])
+
+for i in range(1, 2018):
+    buffer.rotate(-steps - 1)
+    buffer.appendleft(i)
+
+num = 0
+index = 0
+
+for i in range(1, 50_000_000):
+    index = (index + steps) % i
+
+    if index == 0:
+        num = i
+
+    index += 1
+
+print('Part 1:', buffer[1])
+print('Part 2:', num)

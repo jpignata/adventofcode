a991de63394810ee36f8098270a4a5236f09bc45
John Pignata <john@pignata.com>
2019-12-01 00:20:45 -0500

    2019/01

---
+++ 2019/01/solve.py
@@ -0,0 +1,16 @@
+import sys
+
+modules = [int(line) for line in sys.stdin]
+part1, part2 = 0, 0
+
+for module in modules:
+    fuel = module // 3 - 2
+    part1 += fuel
+    part2 += fuel
+
+    while fuel > 0:
+        fuel = max(fuel // 3 - 2, 0)
+        part2 += fuel
+
+print(f'Part 1: {part1}')
+print(f'Part 2: {part2}')

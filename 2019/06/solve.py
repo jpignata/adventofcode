9c9e28888d893279557dba23b297e4ca8c3000f3
John Pignata <john@pignata.com>
2019-12-06 01:02:34 -0500

    2019/06

---
+++ 2019/06/solve.py
@@ -0,0 +1,40 @@
+import sys
+from collections import deque, defaultdict
+
+orbits = dict()
+connections = defaultdict(set)
+part1, part2 = 0, 0
+
+for line in sys.stdin.readlines():
+    planets = line.strip().split(')')
+    connections[planets[0]].add(planets[1])
+    connections[planets[1]].add(planets[0])
+    orbits[planets[1]] = planets[0]
+
+for planet, orbiting in orbits.items():
+    while True:
+        part1 += 1
+
+        if orbiting == 'COM':
+            break
+
+        orbiting = orbits[orbiting]
+
+q = deque([[orbits['YOU'], 0]])
+visited = set()
+
+while q:
+    planet, distance = q.popleft()
+
+    if planet not in visited:
+        visited.add(planet)
+
+        if planet == 'SAN':
+            part2 = distance - 1
+            break
+        else:
+            for connection in connections[planet]:
+                q.append([connection, distance + 1])
+
+print(f'Part 1: {part1}')
+print(f'Part 2: {part2}')

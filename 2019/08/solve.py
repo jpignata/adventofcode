765f859d7de29e2887af1d57169c147994a9b8b0
John Pignata <john@pignata.com>
2019-12-08 00:51:46 -0500

    2019/08

---
+++ 2019/08/solve.py
@@ -0,0 +1,26 @@
+import sys
+
+digits = [int(d) for d in sys.stdin.readline().strip()]
+min_layer = min([digits[i:i + 150] for i in range(0, len(digits), 150)],
+                key=lambda l: l.count(0))
+chunks = [digits[i:i + 25] for i in range(0, len(digits), 25)]
+layers = [chunks[i:i + 6] for i in range(0, len(chunks), 6)]
+image = layers[0].copy()
+
+for i in range(1, len(layers)):
+    for j in range(len(layers[0])):
+        for k in range(len(layers[0][0])):
+            if image[j][k] == 2:
+                image[j][k] = layers[i][j][k]
+
+print(f'Part 1: {min_layer.count(1) * min_layer.count(2)}')
+print('Part 2:')
+
+for row in image:
+    for pixel in row:
+        if pixel == 1:
+            sys.stdout.write('█')
+        else:
+            sys.stdout.write(' ')
+
+    print()

c839c746cd11bc69659684930d587c2660578f3c
John Pignata <john@pignata.com>
2019-12-28 09:09:58 -0500

    2019/19, use extracted intcode Computer

--- 2019/19/solve.py
+++ 2019/19/solve.py
@@ -1,113 +1,13 @@
-import sys
-from collections import deque, defaultdict
-from operator import add
-from os import system
+from collections import defaultdict
 
-
-class Input(Exception):
-    pass
-
-
-class Computer:
-    def __init__(self, program):
-        self.halted = False
-        self.program = program
-        self.inputs = deque([])
-        self.outputs = deque([])
-        self.pointer = 0
-        self.base = 0
-
-    def param(self, opcode, position):
-        modes = list(reversed(opcode[:3]))
-        mode = modes[position - 1]
-
-        try:
-            if mode == '0':
-                return self.program[self.pointer + position]
-            elif mode == '1':
-                return self.pointer + position
-            elif mode == '2':
-                return self.program[self.pointer + position] + self.base
-            else:
-                print(f'Unknown parameter mode: {mode}')
-                exit
-        except IndexError:
-            return None
-
-    def getval(self, address):
-        if address < 0:
-            print(f'Invalid address: {address}')
-            exit()
-
-        return self.program[address]
-
-    def add(self, param1, param2, param3):
-        self.program[param3] = self.getval(param1) + self.getval(param2)
-        self.pointer += 4
-
-    def mul(self, param1, param2, param3):
-        self.program[param3] = self.getval(param1) * self.getval(param2)
-        self.pointer += 4
-
-    def get(self, param1, *_):
-        if len(self.inputs) == 0:
-            raise Input
-        self.program[param1] = self.inputs.popleft()
-        self.pointer += 2
-
-    def put(self, param1, *_):
-        self.outputs.append(self.getval(param1))
-        self.pointer += 2
-
-    def jump_if_true(self, param1, param2, *_):
-        if self.getval(param1) != 0:
-            self.pointer = self.getval(param2)
-        else:
-            self.pointer += 3
-
-    def jump_if_false(self, param1, param2, *_):
-        if self.getval(param1) == 0:
-            self.pointer = self.getval(param2)
-        else:
-            self.pointer += 3
-
-    def less_than(self, param1, param2, param3):
-        self.program[param3] = int(self.getval(param1) < self.getval(param2))
-        self.pointer += 4
-
-    def equals(self, param1, param2, param3):
-        self.program[param3] = int(self.getval(param1) == self.getval(param2))
-        self.pointer += 4
-
-    def set_base(self, param1, *_):
-        self.base += self.getval(param1)
-        self.pointer += 2
-
-    def halt(self, *_):
-        self.halted = True
-
-    def tick(self):
-        operations = {1: self.add, 2: self.mul, 3: self.get, 4: self.put,
-                      5: self.jump_if_true, 6: self.jump_if_false,
-                      7: self.less_than, 8: self.equals, 9: self.set_base,
-                      99: self.halt}
-
-        opcode = str(self.getval(self.pointer)).zfill(5)
-        param1 = self.param(opcode, 1)
-        param2 = self.param(opcode, 2)
-        param3 = self.param(opcode, 3)
-        operation = operations[int(opcode[-2:])]
-
-        operation(param1, param2, param3)
+from intcode import Computer
 
 
 def test(x, y):
-    computer = Computer(program.copy())
+    computer = Computer.load()
     computer.inputs.append(x)
     computer.inputs.append(y)
-
-    while not computer.halted:
-        computer.tick()
+    computer.run()
 
     return computer.outputs.popleft()
 
@@ -123,11 +23,6 @@ def search(size):
 
     return x * 10000 + y
 
-
-program = defaultdict(int)
-
-for i, digit in enumerate(sys.stdin.readline().split(',')):
-    program[i] = int(digit)
 
 total = 0
 grid = defaultdict(int)

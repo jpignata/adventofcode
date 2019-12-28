6fb4a874e1e86d0c725a1b916bd8966e912b7bb3
John Pignata <john@pignata.com>
2019-12-28 09:17:43 -0500

    2019/23, use extracted intcode Computer

--- 2019/23/solve.py
+++ 2019/23/solve.py
@@ -1,114 +1,13 @@
-import sys
-from collections import deque, defaultdict
-from operator import add
-from os import system
+from intcode import Computer, Input
 
 
-class Input(Exception):
-    pass
-
-
-class Computer:
-    def __init__(self, program):
-        self.halted = False
-        self.program = program
-        self.inputs = deque([])
-        self.outputs = deque([])
-        self.pointer = 0
-        self.base = 0
-
-    def param(self, opcode, position):
-        modes = list(reversed(opcode[:3]))
-        mode = modes[position - 1]
-
-        try:
-            if mode == '0':
-                return self.program[self.pointer + position]
-            elif mode == '1':
-                return self.pointer + position
-            elif mode == '2':
-                return self.program[self.pointer + position] + self.base
-            else:
-                raise f'Unknown parameter mode: {mode}'
-        except IndexError:
-            return None
-
-    def getval(self, address):
-        if address < 0:
-            raise f'Invalid address: {address}'
-
-        return self.program[address]
-
-    def add(self, param1, param2, param3):
-        self.program[param3] = self.getval(param1) + self.getval(param2)
-        self.pointer += 4
-
-    def mul(self, param1, param2, param3):
-        self.program[param3] = self.getval(param1) * self.getval(param2)
-        self.pointer += 4
-
-    def get(self, param1, *_):
-        if len(self.inputs) == 0:
-            raise Input
-
-        self.program[param1] = self.inputs.popleft()
-        self.pointer += 2
-
-    def put(self, param1, *_):
-        self.outputs.append(self.getval(param1))
-        self.pointer += 2
-
-    def jump_if_true(self, param1, param2, *_):
-        if self.getval(param1) != 0:
-            self.pointer = self.getval(param2)
-        else:
-            self.pointer += 3
-
-    def jump_if_false(self, param1, param2, *_):
-        if self.getval(param1) == 0:
-            self.pointer = self.getval(param2)
-        else:
-            self.pointer += 3
-
-    def less_than(self, param1, param2, param3):
-        self.program[param3] = int(self.getval(param1) < self.getval(param2))
-        self.pointer += 4
-
-    def equals(self, param1, param2, param3):
-        self.program[param3] = int(self.getval(param1) == self.getval(param2))
-        self.pointer += 4
-
-    def set_base(self, param1, *_):
-        self.base += self.getval(param1)
-        self.pointer += 2
-
-    def halt(self, *_):
-        self.halted = True
-
-    def tick(self):
-        operations = {1: self.add, 2: self.mul, 3: self.get, 4: self.put,
-                      5: self.jump_if_true, 6: self.jump_if_false,
-                      7: self.less_than, 8: self.equals, 9: self.set_base,
-                      99: self.halt}
-
-        opcode = str(self.getval(self.pointer)).zfill(5)
-        param1 = self.param(opcode, 1)
-        param2 = self.param(opcode, 2)
-        param3 = self.param(opcode, 3)
-        operation = operations[int(opcode[-2:])]
-
-        operation(param1, param2, param3)
-
-
-def network(program):
+def network():
     computers = list()
     last = dict()
     x, y = None, None
 
     for i in range(50):
-        computer = Computer(program.copy())
-        computer.inputs.append(i)
-        computers.append(computer)
+        computers.append(Computer.load([i]))
 
     while True:
         for i, computer in enumerate(computers):
@@ -138,16 +37,16 @@ def network(program):
                 computers[0].inputs.append(y)
 
 
-def first(program):
-    for destination, x, y in network(program):
+def first():
+    for destination, x, y in network():
         if destination == 255:
             return y
 
 
-def repeat(program):
+def repeat():
     last = None
 
-    for destination, x, y in network(program):
+    for destination, x, y in network():
         if destination == 0:
             if last == y:
                 return y
@@ -155,10 +54,5 @@ def repeat(program):
             last = y
 
 
-program = defaultdict(int)
-
-for i, digit in enumerate(sys.stdin.readline().split(',')):
-    program[i] = int(digit)
-
-print('Part 1:', first(program))
-print('Part 2:', repeat(program))
+print('Part 1:', first())
+print('Part 2:', repeat())

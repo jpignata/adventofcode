072a8a8de53d579cb61cc2c311846348af4c8694
John Pignata <john@pignata.com>
2019-12-28 08:58:46 -0500

    2019/13, use extracted intcode Computer

--- 2019/13/solve.py
+++ 2019/13/solve.py
@@ -1,13 +1,7 @@
 import curses
-import sys
-import itertools
-from collections import deque, defaultdict
 from operator import itemgetter
-from os import system
 
-
-class Input(Exception):
-    pass
+from intcode import Computer, Input, Halt
 
 
 def draw(screen, score):
@@ -34,110 +28,14 @@ def draw(screen, score):
     stdscr.refresh()
 
 
-class Computer:
-    def __init__(self, program):
-        self.halted = False
-        self.program = program
-        self.inputs = deque([])
-        self.outputs = deque([])
-        self.pointer = 0
-        self.base = 0
-
-    def param(self, opcode, position):
-        modes = list(reversed(opcode[:3]))
-        mode = modes[position - 1]
-
-        try:
-            if mode == '0':
-                return self.program[self.pointer + position]
-            elif mode == '1':
-                return self.pointer + position
-            elif mode == '2':
-                return self.program[self.pointer + position] + self.base
-            else:
-                raise f'Unknown parameter mode: {mode}'
-        except IndexError:
-            return None
-
-    def getval(self, address):
-        if address < 0:
-            raise f'Invalid address: {address}'
-
-        return self.program[address]
-
-    def add(self, param1, param2, param3):
-        self.program[param3] = self.getval(param1) + self.getval(param2)
-        self.pointer += 4
-
-    def mul(self, param1, param2, param3):
-        self.program[param3] = self.getval(param1) * self.getval(param2)
-        self.pointer += 4
-
-    def get(self, param1, *_):
-        if len(self.inputs) == 0:
-            raise Input
-        self.program[param1] = self.inputs.popleft()
-        self.pointer += 2
-
-    def put(self, param1, *_):
-        self.outputs.append(self.getval(param1))
-        self.pointer += 2
-
-    def jump_if_true(self, param1, param2, *_):
-        if self.getval(param1) != 0:
-            self.pointer = self.getval(param2)
-        else:
-            self.pointer += 3
-
-    def jump_if_false(self, param1, param2, *_):
-        if self.getval(param1) == 0:
-            self.pointer = self.getval(param2)
-        else:
-            self.pointer += 3
-
-    def less_than(self, param1, param2, param3):
-        self.program[param3] = int(self.getval(param1) < self.getval(param2))
-        self.pointer += 4
-
-    def equals(self, param1, param2, param3):
-        self.program[param3] = int(self.getval(param1) == self.getval(param2))
-        self.pointer += 4
-
-    def set_base(self, param1, *_):
-        self.base += self.getval(param1)
-        self.pointer += 2
-
-    def halt(self, *_):
-        self.halted = True
-
-    def tick(self):
-        operations = {1: self.add, 2: self.mul, 3: self.get, 4: self.put,
-                      5: self.jump_if_true, 6: self.jump_if_false,
-                      7: self.less_than, 8: self.equals, 9: self.set_base,
-                      99: self.halt}
-
-        opcode = str(self.getval(self.pointer)).zfill(5)
-        param1 = self.param(opcode, 1)
-        param2 = self.param(opcode, 2)
-        param3 = self.param(opcode, 3)
-        operation = operations[int(opcode[-2:])]
-
-        operation(param1, param2, param3)
-
-
-program = defaultdict(int)
-
-for i, digit in enumerate(sys.stdin.readline().split(',')):
-    program[i] = int(digit)
-
-computer = Computer(program.copy())
-
-while not computer.halted:
-    computer.tick()
+computer = Computer.load()
+computer.run()
 
 blocks = sum(1 for i, c in enumerate(computer.outputs)
              if c == 2 and (i + 1) % 3 == 0)
-computer = Computer(program.copy())
+
+
+computer = Computer.load()
 computer.program[0] = 2
 screen = dict()
 score = 0
@@ -159,6 +57,8 @@ while not computer.halted:
             computer.inputs.append(0)
 
         draw(screen, score)
+    except Halt:
+        pass
 
     if len(computer.outputs) == 3:
         x, y, tile_or_score = [computer.outputs.popleft() for _ in range(3)]

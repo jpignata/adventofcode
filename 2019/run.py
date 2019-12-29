f78925315031db24780fc20a800174502d0d078a
John Pignata <john@pignata.com>
2019-12-29 18:21:41 -0500

    Add script to run all days

---
+++ 2019/run.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import os
+import timeit
+
+for directory, _, files in sorted(os.walk('.')):
+    if 'solve.py' in files:
+        print('---', f'Day {directory[2:]}')
+
+        if 'input.txt' in files:
+            cmd = f'cat {directory}/input.txt | python {directory}/solve.py'
+        else:
+            cmd = f'python {directory}/solve.py'
+
+        start = timeit.default_timer()
+        os.system(cmd)
+        elapsed = timeit.default_timer() - start
+        print('---', 'Time:', elapsed, end='\n\n')

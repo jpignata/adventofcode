330c7bb6eaaa57c313f3e0c33832763988862d82
John Pignata <john@pignata.com>
2019-12-31 15:43:50 -0500

    Add hardcoded 2019/17 part 2 solution

--- 2019/17/solve.py
+++ 2019/17/solve.py
@@ -1,40 +1,50 @@
 from operator import add
+from collections import deque
 
 from intcode import Computer
 
-
-def alignment_parameters(grid):
-    moves = ((0, -1), (0, 1), (-1, 0), (1, 0))
-    intersections = []
-
-    for y, row in enumerate(grid):
-        for x, cell in enumerate(row):
-            if cell == '#':
-                for move in moves:
-                    dx, dy = tuple(map(add, move, (x, y)))
-
-                    if 0 <= dy < len(grid) and 0 <= dx < len(grid[dy]):
-                        if grid[dy][dx] == '.':
-                            break
-                else:
-                    intersections.append((x, y))
-
-    return sum(x * y for x, y in intersections)
-
-
 computer = Computer.load()
-grid = []
-row = []
+grid = list()
+row = list()
+intersections = list()
 
 computer.run()
 
 while computer.outputs:
-    char = computer.outputs.popleft()
-
-    if char == 10:
+    if (char := computer.outputs.popleft()) == 10:
         grid.append(row)
-        row = []
+        row = list()
     else:
         row.append(chr(char))
 
-print('Part 1:', alignment_parameters(grid))
+for y, row in enumerate(grid):
+    for x, cell in enumerate(row):
+        if cell == '#':
+            for move in ((0, -1), (0, 1), (-1, 0), (1, 0)):
+                dx, dy = tuple(map(add, move, (x, y)))
+
+                if 0 <= dy < len(grid) and 0 <= dx < len(grid[dy]):
+                    if grid[dy][dx] == '.':
+                        break
+            else:
+                intersections.append((x, y))
+
+print('Part 1:', sum(x * y for x, y in intersections))
+
+program = """
+A,B,A,A,B,C,B,C,C,B
+L,12,R,8,L,6,R,8,L,6
+R,8,L,12,L,12,R,8
+L,6,R,6,L,12
+n
+"""
+
+computer = computer.load()
+computer[0] = 2
+
+for line in program.strip().split('\n'):
+    computer.execute(line)
+
+computer.run()
+
+print('Part 2:', computer.outputs.pop())

11e96de67b5e8e335f2bddb198c89637491a3674
John Pignata <john@pignata.com>
2019-12-30 21:25:57 -0500

    2019/25, automated, sort of
    Kind of faking it with a memory file with all items dropped at the
    security checkpoint. Then I just navigate to it, hard-coded, natch, and
    then try every combination. idk.

--- 2019/25/play.py
+++ 2019/25/play.py
@@ -1,12 +1,21 @@
-from intcode import Computer, Input
+import os
 
-computer = Computer.load(filename='input.txt')
+from intcode import Computer, Input, Halt
+
+computer = Computer.load(filename=f'./{os.path.dirname(__file__)}/input.txt')
 
 while not computer.halted:
     try:
         computer.tick()
     except Input:
         computer.print_screen()
-        computer.execute(input())
 
-# Required items: hypercube, prime number, mouse, wreath
+        if (command := input()) == 'dump':
+            with open('memory.out', 'w') as f:
+                f.write(','.join(str(d) for d in computer.program.values()))
+
+            print('Wrote memory.out.', end='\n\n')
+        else:
+            computer.execute(command)
+    except Halt:
+        computer.print_screen()
---
+++ 2019/25/solve.py
@@ -0,0 +1,43 @@
+import sys
+import os
+import itertools
+import re
+
+from intcode import Computer, Input
+
+
+def run_until_input(computer):
+    try:
+        computer.run()
+    except Input:
+        return
+
+
+items = list()
+computer = Computer.load(filename=f'./{os.path.dirname(__file__)}/memory.out')
+
+for execute in ('west', 'north', 'west', 'west', 'west'):
+    computer.execute(execute)
+
+run_until_input(computer)
+
+for line in computer.screen().split('\n'):
+    if line == 'Items here:':
+        items = list()
+    elif line.startswith('- '):
+        items.append(line[2:])
+
+for i in range(len(items)):
+    for combination in itertools.combinations(items, i):
+        for item in combination:
+            computer.execute(f'take {item}')
+
+        computer.execute('north')
+        run_until_input(computer)
+
+        if match := re.search('get in by typing (\d*)', computer.screen()):
+            print('Part 1:', match[1])
+            break
+
+        for item in combination:
+            computer.execute(f'drop {item}')

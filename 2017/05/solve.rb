f8c2542b8ad0128bdaa6b6b1e093eed02c69e18e
John Pignata <john@pignata.com>
2020-07-04 10:56:57 -0400

    Move ruby examples into chronological tree
    Rather than maintaining a separate root for a different language, just
    use the existing directories.

---
+++ 2017/05/solve.rb
@@ -0,0 +1,22 @@
+def run(offsets)
+  offsets = offsets.dup
+  pointer = 0
+  steps = 0
+  
+  while pointer < offsets.size
+    next_pointer = pointer + offsets[pointer]
+    offsets[pointer] = yield(offsets[pointer])  
+    pointer = next_pointer
+    steps += 1
+  end
+  
+  steps  
+end
+
+offsets = ARGF.each_line.map(&:to_i)
+
+part1 = run(offsets) { |offset| offset + 1 }
+part2 = run(offsets) { |offset| offset >= 3 ? offset - 1 : offset + 1 }
+
+puts "Part 1: #{part1}"
+puts "Part 2: #{part2}"

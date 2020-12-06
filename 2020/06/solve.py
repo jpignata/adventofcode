13ed6a60f992107db02982b63ce35f4033564c0c
John Pignata <john@pignata.com>
2020-12-06 10:19:46 -0500

    2020/06, clean up

--- 2020/06/solve.py
+++ 2020/06/solve.py
@@ -1,14 +1,14 @@
 import sys
-from functools import reduce
 
-part1, part2, group = 0, 0, []
+part1, part2 = 0, 0
+group = []
 
 for line in [line.strip() for line in sys.stdin.readlines()] + ['']:
-    if len(line):
+    if line:
         group.append(set(line))
     else:
-        part1 += len(reduce(lambda x, y: x | y, group))
-        part2 += len(reduce(lambda x, y: x & y, group))
+        part1 += len(set.intersection(*group))
+        part2 += len(set.union(*group))
         group = []
 
 print('Part 1:', part1)

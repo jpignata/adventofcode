911577ff504bda21cd0e1bc40848a77fab15404a
John Pignata <john@pignata.com>
2020-12-15 05:44:29 -0500

    2020/15, brute force
    It runs in 5 seconds so good enough. Will look for the pattern later.

---
+++ 2020/15/solve.py
@@ -0,0 +1,17 @@
+import sys
+
+
+def play(numbers, rounds):
+    seen = {number: i + 1 for i, number in enumerate(numbers)}
+    spoken = numbers[-1]
+
+    for turn in range(len(numbers), rounds):
+        seen[spoken], spoken = turn, turn - seen.get(spoken, turn)
+
+    return spoken
+
+
+numbers = [int(line) for line in sys.stdin.readline().split(',')]
+
+print('Part 1:', play(numbers, 2020))
+print('Part 2:', play(numbers, 30000000))

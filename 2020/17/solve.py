0de870267e514cc873933a5b2239f361331af982
John Pignata <john@pignata.com>
2020-12-17 17:16:35 -0500

    2020/17, optimize
    Don't bother iterating over inactive cubes and just produce a
    set of active cubes. This substantially speeds up the
    search process.
    
    - Remove caching which is no longer improving performnace

--- 2020/17/solve.py
+++ 2020/17/solve.py
@@ -1,41 +1,27 @@
 import sys
 from itertools import product
-from functools import cache
 
 
 def simulate(cubes, dims, cycles=6):
-    cubes = {tuple(pos[i] if i < len(pos) else 0 for i in range(dims)): active
-             for pos, active in cubes.items()}
-    deltas = [delta for delta in product(*((-1, 0, 1) for _ in range(dims)))
-              if any(delta)]
-
-    @cache
-    def neighbors(pos):
-        return [tuple(sum(p) for p in zip(pos, delta)) for delta in deltas]
+    cubes = {cube + (0,) * (dims - len(cube)) for cube in cubes}
+    deltas = [diff for diff in product((-1, 0, 1), repeat=dims) if any(diff)]
 
     for _ in range(cycles):
-        next_cubes = cubes.copy()
+        neighbors = {}
 
-        for pos in list(cubes):
-            for neighbor in neighbors(pos):
-                cubes[neighbor] = cubes.get(neighbor, 0)
+        for cube in cubes:
+            for delta in deltas:
+                neighbor = tuple(sum(p) for p in zip(cube, delta))
+                neighbors[neighbor] = neighbors.get(neighbor, 0) + 1
 
-        for pos in cubes:
-            active = sum(cubes.get(neighbor, 0) for neighbor in neighbors(pos))
+        cubes = {neighbor for neighbor, active in neighbors.items()
+                 if (neighbor in cubes and active == 2) or active == 3}
 
-            if cubes[pos] == 1 and active != 2 and active != 3:
-                next_cubes[pos] = 0
-            elif cubes[pos] == 0 and active == 3:
-                next_cubes[pos] = 1
-
-        cubes = next_cubes
-
-    return sum(cubes.values())
+    return len(cubes)
 
 
-cubes = {(x, y): int(cell == '#')
-         for y, line in enumerate(sys.stdin.readlines())
-         for x, cell in enumerate(line.strip())}
+cubes = {(x, y) for y, line in enumerate(sys.stdin.readlines())
+         for x, cell in enumerate(line.strip()) if cell == '#'}
 
 print('Part 1:', simulate(cubes, 3))
 print('Part 2:', simulate(cubes, 4))

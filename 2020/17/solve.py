7ee398b92299f89a5cd2db70c08f200cab2c371f
John Pignata <john@pignata.com>
2020-12-17 10:22:43 -0500

    2020/17, generalized

--- 2020/17/solve.py
+++ 2020/17/solve.py
@@ -1,66 +1,43 @@
 import sys
 from itertools import product
-from collections import defaultdict
+from functools import lru_cache
 
 
-def simulate3(cubes, cycles=6):
-    adjacent = [xyz for xyz in product((-1, 0, 1), (-1, 0, 1), (-1, 0, 1))
-                if xyz != (0, 0, 0)]
+def simulate(cubes, dims, cycles=6):
+    cubes = {tuple(0 if i >= len(pos) else pos[i] for i in range(dims)): cube
+            for pos, cube in cubes.items()}
+    adjacent = [pos for pos in product(*((-1, 0, 1) for _ in range(dims)))
+                if not all(p == 0 for p in pos)]
+
+    @lru_cache(maxsize=None)
+    def neighbors(pos):
+        return [tuple(c + d for c, d in zip(pos, adj)) for adj in adjacent]
 
     for _ in range(cycles):
         next_cubes = cubes.copy()
 
-        for (x, y, z), cube in list(cubes.items()):
-            neighbors = sum(cubes[(x+dx, y+dy, z+dz)]
-                            for dx, dy, dz in adjacent)
+        for pos in list(cubes):
+            for neighbor in neighbors(pos):
+                cubes[neighbor] = cubes.get(neighbor, 0)
 
-        for (x, y, z), cube in list(cubes.items()):
-            neighbors = sum(cubes[(x+dx, y+dy, z+dz)]
-                            for dx, dy, dz in adjacent)
+        for pos in cubes:
+            active = sum(cubes.get(neighbor, 0) for neighbor in neighbors(pos))
 
-            if cube == 1 and neighbors not in (2, 3):
-                next_cubes[(x, y, z)] = 0
-            elif cube == 0 and neighbors == 3:
-                next_cubes[(x, y, z)] = 1
+            if cubes[pos] == 1 and active != 2 and active != 3:
+                next_cubes[pos] = 0
+            elif cubes[pos] == 0 and active == 3:
+                next_cubes[pos] = 1
 
         cubes = next_cubes
 
     return sum(cubes.values())
 
 
-def simulate4(cubes, cycles=6):
-    adjacent = [xyzw for xyzw in
-                product((-1, 0, 1), (-1, 0, 1), (-1, 0, 1), (-1, 0, 1))
-                if xyzw != (0, 0, 0, 0)]
-
-    for _ in range(cycles):
-        next_cubes = cubes.copy()
-
-        for (x, y, z, w), cube in list(cubes.items()):
-            neighbors = sum(cubes[(x+dx, y+dy, z+dz, w+dw)]
-                            for dx, dy, dz, dw in adjacent)
-
-        for (x, y, z, w), cube in list(cubes.items()):
-            neighbors = sum(cubes[(x+dx, y+dy, z+dz, w+dw)]
-                            for dx, dy, dz, dw in adjacent)
-
-            if cube == 1 and neighbors not in (2, 3):
-                next_cubes[(x, y, z, w)] = 0
-            elif cube == 0 and neighbors == 3:
-                next_cubes[(x, y, z, w)] = 1
-
-        cubes = next_cubes
-
-    return sum(cubes.values())
-
-
-cubes3 = defaultdict(int)
-cubes4 = defaultdict(int)
+cubes = {}
 
 for y, line in enumerate(sys.stdin.readlines()):
     for x, cell in enumerate(line.strip()):
-        cubes3[(x, y, 0)] = 1 if cell == '#' else 0
-        cubes4[(x, y, 0, 0)] = 1 if cell == '#' else 0
+        cubes[(x, y)] = 1 if cell == '#' else 0
 
-print('Part 1:', simulate3(cubes3))
-print('Part 2:', simulate4(cubes4))
+print('Part 1:', simulate(cubes, 3))
+print('Part 2:', simulate(cubes, 4))

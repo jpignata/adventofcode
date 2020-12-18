f156d740cfa35a824b6d19a99aae6a8bd4a6ea48
John Pignata <john@pignata.com>
2020-12-18 09:29:55 -0500

    2020/18

--- 2020/18/solve.py
+++ 2020/18/solve.py
@@ -1,37 +1,32 @@
-import re
-import sys
-from collections import deque
 import operator
+import sys
 
-operations = {'+': operator.add, '*': operator.mul}
-order = ['+', '*']
-part1, part2 = 0, 0
 
-for line in sys.stdin.readlines():
-    expr = re.findall(r'[+*()]|\d+', line)
-    output = []
-    ops = []
+def evaluate(expr, order):
+    operators = {'+': operator.add, '*': operator.mul}
+    values, ops = [], []
 
     for token in expr:
-        print(output, ops)
         if token.isdigit():
-            output.append(int(token))
+            values.append(int(token))
         elif token == '(':
             ops.append(token)
         elif token == ')':
             while ((op := ops.pop()) != '('):
-                output.append(operations[op](output.pop(), output.pop()))
-        elif token in operations:
-            while (ops and ops[-1] != '(' and order.index(token) >= order.index(ops[-1])):
-                output.append(operations[ops.pop()](output.pop(), output.pop()))
+                values.append(operators[op](values.pop(), values.pop()))
+        elif token in operators:
+            while (ops and ops[-1] != '(' and order[token] <= order[ops[-1]]):
+                values.append(operators[ops.pop()](values.pop(), values.pop()))
 
             ops.append(token)
 
-
     while ops:
-        output.append(operations[ops.pop()](output.pop(), output.pop()))
+        values.append(operators[ops.pop()](values.pop(), values.pop()))
 
-    print(output)
-    part1 += output[-1]
+    return values[0]
 
-print('Part 1:', part1)
+
+exprs = sys.stdin.readlines()
+
+print('Part 1:', sum(evaluate(expr, {'+': 0, '*': 0}) for expr in exprs))
+print('Part 2:', sum(evaluate(expr, {'+': 1, '*': 0}) for expr in exprs))

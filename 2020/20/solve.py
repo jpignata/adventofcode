fbfd1586965476a150b8578edc0a235dd35c9a39
John Pignata <john@pignata.com>
2020-12-20 22:04:22 -0500

    2020/20, clean-up

--- 2020/20/solve.py
+++ 2020/20/solve.py
@@ -2,100 +2,72 @@ import sys
 from math import sqrt
 
 
-def top(tile):
-    return tile[0]
+def variations(grid):
+    def flipud(grid): return list(grid[::-1])
+    def fliplr(grid): return [list(row[::-1]) for row in grid]
+    def rotate(grid): return [list(row) for row in zip(*grid[::-1])]
 
+    res = []
 
-def bottom(tile):
-    return tile[-1]
+    for _ in range(2):
+        res.extend([grid, flipud(grid), fliplr(grid), flipud(fliplr(grid))])
+        grid = rotate(grid)
 
-
-def left(tile):
-    return [row[0] for row in tile]
-
-
-def right(tile):
-    return [row[-1] for row in tile]
-
-
-def fliplr(tile):
-    return [list(row[::-1]) for row in tile]
-
-
-def flipud(tile):
-    return list(tile[::-1])
-
-
-def rot90(tile):
-    return [list(row) for row in zip(*tile[::-1])]
+    return res
 
 
 def connect(tiles):
     size = int(sqrt(len(tiles)))
-    s = []
+    stack = [([variant], [tile_id]) for tile_id, variants in tiles.items()
+             for variant in variants]
 
-    for tile_id, variants in tiles.items():
-        for variant in variants:
-            s.append(([variant], [tile_id]))
-
-    while s:
-        placed, tile_ids = s.pop()
-        found = len(placed)
+    while stack:
+        cand, ids = stack.pop()
+        found = len(cand)
+        y, x = divmod(found, size)
 
         if found == len(tiles):
-            checksum = tile_ids[0] * tile_ids[size - 1] * \
-                       tile_ids[size * -1] * tile_ids[-1]
-            return placed, checksum
-        else:
-            y, x = divmod(len(placed), size)
+            return cand, ids[0] * ids[size - 1] * ids[size * -1] * ids[-1]
 
-            for other_id, variants in tiles.items():
-                if other_id not in tile_ids:
-                    for v in variants:
-                        if x > 0 and right(placed[found - 1]) != left(v):
-                            continue
+        for other_id, others in tiles.items():
+            if other_id not in ids:
+                for other in others:
+                    if x > 0 and ([r[0] for r in other] !=
+                                  [r[-1] for r in cand[found - 1]]):
+                        continue
 
-                        if y > 0 and bottom(placed[found - size]) != top(v):
-                            continue
+                    if y > 0 and other[0] != cand[found - size][-1]:
+                        continue
 
-                        s.append((placed + [v], tile_ids + [other_id]))
+                    stack.append((cand + [other], ids + [other_id]))
 
 
 def hunt(tiles):
-    monster = ((0, 0), (1, 1), (4, 1), (7, 1), (10, 1), (13, 1), (16, 1),
-               (5, 0), (6, 0), (11, 0), (12, 0), (17, 0), (18, 0), (19, 0),
-               (18, -1))
+    shape = ((0, 0), (1, 1), (4, 1), (5, 0), (6, 0), (7, 1), (10, 1), (11, 0),
+             (12, 0), (13, 1), (16, 1), (17, 0), (18, -1), (18, 0), (19, 0))
     multiplier = int(sqrt(len(tiles)))
     size = (len(tiles[0]) - 2)
-    maxx, maxy = len(tiles[0][0][0]), len(tiles[0][0])
-    image = [[None] * (size * multiplier) for _ in range(size * multiplier)]
+    grid = [[None] * (size * multiplier) for _ in range(size * multiplier)]
 
-    for y, row in enumerate(image):
-        for x in range(len(image[0])):
-            ct = ((y // size) * multiplier) + (x // size)
+    for y in range(len(grid)):
+        for x in range(len(grid[0])):
+            ctile = y // size * multiplier + x // size
             cy = y % size + 1
             cx = x % size + 1
-            image[y][x] = tiles[ct][cy][cx]
+            grid[y][x] = tiles[ctile][cy][cx]
 
-    for _ in range(2):
-        images = [image, flipud(image), fliplr(image), flipud(fliplr(image))]
+    total = sum(1 for row in grid for c in row if c == '#')
+    found = 0
 
-        for variant in images:
-            found = 0
+    for image in variations(grid):
+        for y, row in enumerate(image):
+            for x, cell in enumerate(row):
+                found += all(0 <= y+dy < len(image) and
+                             0 <= x+dx < len(image[0]) and
+                             image[y+dy][x+dx] == '#' for dx, dy in shape)
 
-            for y, row in enumerate(variant):
-                for x, cell in enumerate(row):
-                    if cell == '#':
-                        found += all(0 <= y+dy < len(variant) and
-                                     0 <= x+dx < len(variant[0]) and
-                                     variant[y+dy][x+dx] == '#'
-                                     for dx, dy in monster)
-
-            if found > 0:
-                total = sum(1 for row in variant for c in row if c == '#')
-                return total - (len(monster) * found)
-
-        image = rot90(image)
+        if found:
+            return total - len(shape) * found
 
 
 tiles = {}
@@ -108,16 +80,9 @@ while (line := sys.stdin.readline()):
         while ((line := sys.stdin.readline().strip())):
             tile.append(list(line))
 
-        tiles[tile_id] = []
+        tiles[tile_id] = variations(tile)
 
-        for _ in range(2):
-            tiles[tile_id].append(tile)
-            tiles[tile_id].append(fliplr(tile))
-            tiles[tile_id].append(flipud(tile))
-            tiles[tile_id].append(fliplr(flipud(tile)))
-            tile = rot90(tile)
-
-connected, checksum = connect(tiles)
+image, checksum = connect(tiles)
 
 print('Part 1:', checksum)
-print('Part 2:', hunt(connected))
+print('Part 2:', hunt(image))

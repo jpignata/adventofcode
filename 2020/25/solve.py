23d86728aadb1b6fb7077a52ad3702b6a12e1fcc
John Pignata <john@pignata.com>
2020-12-26 11:18:21 -0500

    2020/25, add input

--- 2020/25/solve.py
+++ 2020/25/solve.py
@@ -1,26 +1,28 @@
+import sys
+
+
 def find(number):
     result = 1
-    loop_size = 0
+    size = 0
 
     while result != number:
         result *= 7
         result %= 20201227
-        loop_size += 1
+        size += 1
 
-    return loop_size
+    return size
 
 
-def calculate(loop_size, number):
+def calculate(size, number):
     result = 1
 
-    for _ in range(loop_size):
+    for _ in range(size):
         result *= number
         result %= 20201227
 
     return result
 
 
-public_keys = [10212254, 12577395]
-loop_sizes = [find(key) for key in public_keys]
+public_keys = [int(line) for line in sys.stdin.readlines()]
 
-print('Part 1:', calculate(loop_sizes[0], public_keys[1]))
+print('Part 1:', calculate(find(public_keys[0]), public_keys[1]))

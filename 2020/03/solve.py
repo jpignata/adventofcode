88ffec3b36bf24bd5642e27e5bc66d1a2e035d3a
John Pignata <john@pignata.com>
2020-12-27 00:18:27 -0500

    2020/03, simplify

--- 2020/03/solve.py
+++ 2020/03/solve.py
@@ -3,18 +3,10 @@ from math import prod
 
 
 def ride(grid, sx, sy):
-    trees, x, y = 0, 0, 0
-
-    while y < len(grid):
-        trees += grid[y][x % len(grid[0])] == '#'
-        x += sx
-        y += sy
-
-    return trees
+    return sum(row[(sx * i) % len(row)] == '#' for i, row in enumerate(grid[::sy]))
 
 
 grid = [line.strip() for line in sys.stdin.readlines()]
-slopes = (1, 1), (3, 1), (5, 1), (7, 1), (1, 2)
 
 print('Part 1:', ride(grid, 3, 1))
-print('Part 2:', prod(ride(grid, x, y) for (x, y) in slopes))
+print('Part 2:', prod(ride(grid, x, y) for (x, y) in ((1, 1), (3, 1), (5, 1), (7, 1), (1, 2))))

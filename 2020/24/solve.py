f14d5a69931d19459d788b919fc36e2728594b17
John Pignata <john@pignata.com>
2021-01-01 20:01:02 -0500

    2020/24, optimize slightly
    - Use axial coordinates instead of cube coordinates as this is easier
      for me to understand
    - Use a regexp to make the initial parsing easier

--- 2020/24/solve.py
+++ 2020/24/solve.py
@@ -1,41 +1,37 @@
+import re
 import sys
-from collections import defaultdict
 
-dirs = {'nw': (0, 1, -1), 'ne': (1, 0, -1), 'sw': (-1, 0, 1), 'se': (0, -1, 1),
-        'w': (-1, 1, 0), 'e': (1, -1, 0)}
+dirs = {'nw': (0, -1), 'ne': (1, -1), 'sw': (-1, 1), 'se': (0, 1), 'w': (-1, 0), 'e': (1, 0)}
+tokens = re.compile('|'.join(dirs))
 start = set()
 
 for flip in sys.stdin.readlines():
-    current = (0, 0, 0)
+    pos = 0, 0
 
-    while flip.strip():
-        for direction in dirs:
-            if (token := flip[:len(direction)]) == direction:
-                current = tuple(sum(p) for p in zip(current, dirs[token]))
-                flip = flip[len(direction):]
-                break
+    for direction in tokens.findall(flip):
+        pos = tuple(c + r for c, r in zip(pos, dirs[direction]))
 
-    if current in start:
-        start.remove(current)
+    if pos in start:
+        start.remove(pos)
     else:
-        start.add(current)
+        start.add(pos)
 
-black_tiles = set(start)
+tiles = set(start)
 
 for i in range(100):
-    neighbors = defaultdict(int)
+    neighbors = {}
     next_tiles = set()
 
-    for tile in black_tiles:
+    for tile in tiles:
         for delta in dirs.values():
-            neighbor = tuple(sum(p) for p in zip(tile, delta))
-            neighbors[neighbor] += 1
+            neighbor = tuple(c + r for c, r in zip(tile, delta))
+            neighbors[neighbor] = neighbors.get(neighbor, 0) + 1
 
     for neighbor, flipped in neighbors.items():
-        if (neighbor in black_tiles and flipped == 1) or flipped == 2:
+        if (neighbor in tiles and flipped == 1) or flipped == 2:
             next_tiles.add(neighbor)
 
-    black_tiles = next_tiles
+    tiles = next_tiles
 
 print('Part 1:', len(start))
-print('Part 2:', len(black_tiles))
+print('Part 2:', len(tiles))

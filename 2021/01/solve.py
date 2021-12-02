4b362117e23093062c76afeb7d93104b59e2371e
John Pignata <pignataj@amazon.com>
2021-12-02 11:31:35 -0500

    2021/01, reconsidered
    I saw someone mention that you don't need to sum the window. This makes
    complete sense given you're subtracting the data point leaving the
    window, and adding the data point entering. Thus, you only need to
    compare the edges. Took this hint and applied zip to make the solution
    smaller.

--- 2021/01/solve.py
+++ 2021/01/solve.py
@@ -1,20 +1,11 @@
-import math
 import sys
 
+
+def find(measurements, offset):
+    return sum(x > y for x, y in zip(measurements[offset:], measurements))
+
+
 measurements = [int(line) for line in sys.stdin]
-count = [0, 0]
-prev = (math.inf, math.inf)
 
-for i, measurement in enumerate(measurements):
-    window = sum(measurements[i:i+3])
-
-    if measurement > prev[0]:
-        count[0] += 1
-
-    if window > prev[1]:
-        count[1] += 1
-
-    prev = (measurement, window)
-
-print('Part 1:', count[0])
-print('Part 2:', count[1])
+print('Part 1:', find(measurements, 1))
+print('Part 2:', find(measurements, 3))

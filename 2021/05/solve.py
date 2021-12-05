bd8f3c597127b1134fac2623ce4d66ccc53b6518
John Pignata <pignataj@amazon.com>
2021-12-05 15:15:44 -0500

    2021/04, simplify
    I wanted to find a way to generate the lines one way regardless of how
    many items were in the individual coordinate ranges. itertools'
    zip_longest fits the bill -- once it runs out of any side of the
    iteration, it'll use a fill value.

--- 2021/05/solve.py
+++ 2021/05/solve.py
@@ -1,7 +1,7 @@
 import re
 import sys
 from collections import Counter
-from itertools import product
+from itertools import zip_longest
 
 
 def coords(p1, p2):
@@ -17,13 +17,13 @@ for line in sys.stdin:
     x1, y1, x2, y2 = [int(number) for number in re.findall(r'\d+', line)]
     rangex = coords(x1, x2)
     rangey = coords(y1, y2)
+    fillvalue = x1 if x1 == x2 else y1
+    points = list(zip_longest(rangex, rangey, fillvalue=fillvalue))
 
     if x1 == x2 or y1 == y2:
-        points = list(product(rangex, rangey))
         part1.update(points)
-        part2.update(points)
-    else:
-        part2.update(zip(rangex, rangey))
+
+    part2.update(points)
 
 print('Part 1:', sum(count > 1 for count in part1.values()))
 print('Part 2:', sum(count > 1 for count in part2.values()))

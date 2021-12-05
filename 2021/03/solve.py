11fec72ae1e73a0a20c8fda7006fa62ddb192419
John Pignata <pignataj@amazon.com>
2021-12-04 22:57:39 -0500

    2021/03, clean-up
    Use the term "strategy" instead of "disposition" and ditch the
    unnecessary `IntEnum`. Also gets rid of some unnecessary extractions and
    simplifies a couple of things.

--- 2021/03/solve.py
+++ 2021/03/solve.py
@@ -1,43 +1,39 @@
 import sys
 from collections import Counter
-from enum import IntEnum
+
+LEAST_COMMON = -1
+MOST_COMMON = 0
 
 
-class Disposition(IntEnum):
-    LEAST_COMMON = -1
-    MOST_COMMON = 0
+def to_decimal(bits):
+    return int(''.join(bits), 2)
 
 
-def to_decimal(binary):
-    return int(''.join(binary), 2)
-
-
-def find(numbers, disposition, *, tiebreaker):
+def find(numbers, *, strategy, tiebreaker):
     for position in range(len(numbers[0])):
-        rotated = list(zip(*numbers[::-1]))
-        counts = Counter(rotated[position]).most_common()
+        cols = list(zip(*numbers))
+        counts = Counter(cols[position]).most_common()
 
         if len(counts) == 1:
             target = counts[0][0]
         elif counts[0][1] == counts[1][1]:
             target = tiebreaker
         else:
-            target = counts[disposition][0]
+            target = counts[strategy][0]
 
         numbers = [number for number in numbers if number[position] == target]
 
         if len(numbers) == 1:
-            return to_decimal(numbers[0])
+            return numbers[0]
 
 
 numbers = [list(line.strip()) for line in sys.stdin]
-rotated = list(zip(*numbers[::-1]))
-half = len(rotated[0]) // 2
+cols = list(zip(*numbers))
 
-gamma = ['1' if bits.count('1') > half else '0' for bits in rotated]
+gamma = ['1' if bits.count('1') > len(cols[0]) / 2 else '0' for bits in cols]
 epsilon = ['0' if bit == '1' else '1' for bit in gamma]
-generator = find(numbers, Disposition.MOST_COMMON, tiebreaker='1')
-scrubber = find(numbers, Disposition.LEAST_COMMON, tiebreaker='0')
+generator = find(numbers, strategy=MOST_COMMON, tiebreaker='1')
+scrubber = find(numbers, strategy=LEAST_COMMON, tiebreaker='0')
 
 print('Part 1:', to_decimal(gamma) * to_decimal(epsilon))
-print('Part 2:', generator * scrubber)
+print('Part 2:', to_decimal(generator) * to_decimal(scrubber))

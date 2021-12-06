92aefda84a012bdc33e735e5f7770888b2d51936
John Pignata <pignataj@amazon.com>
2021-12-05 21:26:25 -0800

    2021/06

---
+++ 2021/06/solve.py
@@ -0,0 +1,24 @@
+import sys
+from collections import Counter
+
+
+def solve(fish, days):
+    for i in range(days):
+        next_fish = Counter()
+
+        for age, count in fish.items():
+            if age == 0:
+                next_fish[6] += count
+                next_fish[8] += count
+            else:
+                next_fish[age - 1] += count
+
+        fish = next_fish
+
+    return sum(fish.values())
+
+
+fish = Counter(int(fish) for fish in sys.stdin.readline().split(','))
+
+print('Part 1:', solve(fish, 80))
+print('Part 2:', solve(fish, 256))

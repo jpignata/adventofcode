083d03bd57aecdd736e909cd876036bdbeb3936f
John Pignata <john@pignata.com>
2021-12-12 12:03:57 -0500

    2021/12

---
+++ 2021/12/solve.py
@@ -0,0 +1,40 @@
+import sys
+from collections import defaultdict, Counter
+
+
+def search(max_visits=1):
+    s = [[['end'], Counter()]]
+    paths = 0
+
+    while s:
+        path, counts = s.pop()
+
+        for cave in edges[path[-1]]:
+            if cave == 'start':
+                paths += 1
+            elif cave != 'end':
+                if cave[0].islower():
+                    if any(count == max_visits for count in counts.values()):
+                        allowed = 1
+                    else:
+                        allowed = max_visits
+
+                    if counts[cave] < allowed:
+                        next_counts = counts.copy()
+                        next_counts[cave] += 1
+                        s.append([path + [cave], next_counts])
+                else:
+                    s.append([path + [cave], counts])
+
+    return paths
+
+
+edges = defaultdict(list)
+
+for line in sys.stdin:
+    cave1, cave2 = line.strip().split('-')
+    edges[cave1].append(cave2)
+    edges[cave2].append(cave1)
+
+print('Part 1:', search())
+print('Part 2:', search(2))

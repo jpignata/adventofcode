2ba74ad8e02a787655fc0c694ad68131641831bc
John Pignata <pignataj@amazon.com>
2021-12-16 16:21:22 -0500

    2021/16, continued clean-up
    Instead of my own reader, use StringIO which does basically the same
    thing.

--- 2021/16/solve.py
+++ 2021/16/solve.py
@@ -1,66 +1,44 @@
 import sys
+from operator import lt, gt, eq
+from io import StringIO
 from math import prod
 
 
-class Reader(object):
-    def __init__(self, s):
-        self.s = s
-        self.pointer = 0
-
-    def read(self, size):
-        segment = self.s[self.pointer:self.pointer+size]
-        self.pointer += size
-        return segment
-
-    def read_decimal(self, size):
-        return int(''.join(self.read(size)), 2)
-
-
 def parse(reader):
-    versions = reader.read_decimal(3)
-    type = reader.read_decimal(3)
+    def decode():
+        version, literal = parse(reader)
+        versions.extend(version)
+        literals.append(literal)
 
-    if type == 4:
-        literal = ""
+    literals = []
+    versions = [int(reader.read(3), 2)]
+    packet_type = int(reader.read(3), 2)
+
+    if packet_type == 4:
+        literal = ''
 
         while (segment := reader.read(5)):
             literal += segment[1:]
 
-            if segment[0] == '0':
+            if int(segment[0]) == 0:
                 break
 
-        return (versions, int(''.join(literal), 2))
+        return versions, int(literal, 2)
+    elif (length_type := int(reader.read(1))) == 0:
+        read_to = int(reader.read(15), 2) + reader.tell()
 
-    decoded = []
-    literals = []
+        while reader.tell() != read_to:
+            decode()
+    elif length_type == 1:
+        for _ in range(int(reader.read(11), 2)):
+            decode()
 
-    if reader.read_decimal(1):
-        for _ in range(reader.read_decimal(11)):
-            decoded.append(parse(reader))
-    else:
-        bits_to_read = reader.read_decimal(15)
-        start = reader.pointer
-
-        while reader.pointer - start < bits_to_read:
-            decoded.append(parse(reader))
-
-    for version, literal in decoded:
-        versions += version
-        literals.append(literal)
-
-    return (versions, operations[type](literals))
+    return versions, operations[packet_type](literals)
 
 
-bits = [bin(int(c, 16))[2:].zfill(4) for c in sys.stdin.readline().strip()]
-operations = {
-    0: lambda literals: sum(literals),
-    1: lambda literals: prod(literals),
-    2: lambda literals: min(literals),
-    3: lambda literals: max(literals),
-    5: lambda literals: int(literals[0] > literals[1]),
-    6: lambda literals: int(literals[0] < literals[1]),
-    7: lambda literals: int(literals[0] == literals[1])}
-versions, result = parse(Reader(''.join(bits)))
+operations = {0: sum, 1: prod, 2: min, 3: max, 5: lambda x: gt(*x), 6: lambda x: lt(*x), 7: lambda x: eq(*x)}
+bits = ''.join(bin(int(c, 16))[2:].zfill(4) for c in sys.stdin.read().strip())
+versions, result = parse(StringIO(bits))
 
-print('Part 1:', versions)
+print('Part 1:', sum(versions))
 print('Part 2:', result)

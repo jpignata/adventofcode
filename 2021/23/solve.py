6b89e3d29f1c350e4a7243bd161e6dafea0c0bec
John Pignata <pignataj@amazon.com>
2021-12-23 23:57:14 -0500

    2021/23, 50% correct

--- 2021/23/solve.py
+++ 2021/23/solve.py
@@ -1,13 +1,9 @@
 import sys
-from heapq import heappush, heappop
+from collections import defaultdict
 from copy import deepcopy
+from heapq import heappush, heappop
+from math import inf
 from string import ascii_uppercase
-
-
-def pprint(grid):
-    for row in grid:
-        print(''.join(row))
-    print()
 
 
 def freeze(grid):
@@ -18,22 +14,27 @@ def distance(p1, p2):
     return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
 
 
-def inplace(grid):
-    found = 0
+def heuristic(grid):
+    score = 0
 
-    for i, amphipod in enumerate(amphipods):
-        room = rooms[i]
-        spaces = [row[room] for row in grid if row[room] in ascii_uppercase]
+    for y, row in enumerate(grid):
+        for x, space in enumerate(row):
+            if space in ascii_uppercase:
+                room = rooms[ord(space) - 65]
+                spaces = [row[room] for row in grid
+                          if row[room] in ascii_uppercase]
 
-        if all(space == amphipod for space in spaces):
-            found += len(spaces)
+                if y == 1 and x != room:
+                    score += distance((x, y), (room, 1))
+                elif y != 1 and (x != room or len(set(spaces)) > 1):
+                    score += distance((x, y), (room, 1)) + 1
 
-    return found
+    return score
 
 
 def moves(grid, x, y):
     valid = (1, 2, 4, 6, 8, 10, 11)
-    move_cost = energy[ord(grid[y][x]) - 65]
+    cost = energy[ord(grid[y][x]) - 65]
     destination = rooms[ord(grid[y][x]) - 65]
 
     if x == destination and y == 3:
@@ -47,14 +48,16 @@ def moves(grid, x, y):
 
             while grid[y][nx + dx] == '.':
                 if nx + dx == destination:
-                    room = [grid[ny][nx + dx] for ny in range(y, len(grid)) if grid[ny][nx + dx] in ascii_uppercase]
+                    room = [grid[ny][nx + dx] for ny in range(y, len(grid))
+                            if grid[ny][nx + dx] in ascii_uppercase]
 
                     if all(space == grid[y][x] for space in room) or not room:
+                        ny = len(grid) - len(room) - 2
                         next_grid = deepcopy(grid)
-                        next_grid[len(grid) - len(room) - 2][nx + dx] = grid[y][x]
+                        next_grid[ny][nx + dx] = grid[y][x]
                         next_grid[y][x] = '.'
 
-                        yield distance((x, y), (nx + dx, len(grid) - len(room) - 2)) * move_cost, next_grid
+                        yield distance((x, y), (nx + dx, ny)) * cost, next_grid
 
                 nx += dx
     else:
@@ -67,40 +70,48 @@ def moves(grid, x, y):
             nx = x
 
             for dx in (1, -1):
-                while grid[ny][nx + dx] == '.': 
+                while grid[ny][nx + dx] == '.':
                     if nx + dx in valid:
                         next_grid = deepcopy(grid)
                         next_grid[ny][nx + dx] = grid[y][x]
                         next_grid[y][x] = '.'
 
-                        yield distance((x, y), (nx + dx, ny)) * move_cost, next_grid
-                    
+                        yield distance((x, y), (nx + dx, ny)) * cost, next_grid
+
                     nx += dx
 
 
+def search(grid):
+    visited = defaultdict(lambda: inf)
+    visited[freeze(grid)] = 0
+    q = [(0, 0, grid)]
+
+    while q:
+        _, total, grid = heappop(q)
+        visited[freeze(grid)] = total
+
+        for i, amphipod in enumerate(amphipods):
+            if grid[2][rooms[i]] != amphipod or grid[3][rooms[i]] != amphipod:
+                break
+        else:
+            return total
+
+        for y, row in enumerate(grid):
+            for x, space in enumerate(row):
+                if space in ascii_uppercase:
+                    for cost, next_grid in moves(grid, x, y):
+                        if total + cost < visited[freeze(next_grid)]:
+                            score = heuristic(next_grid)
+                            heappush(q, (score, total + cost, next_grid))
+
 
 grid = [[c for c in line.rstrip()] for line in sys.stdin]
+second_grid = deepcopy(grid)
+second_grid.insert(3, list('  #D#B#A#C#'))
+second_grid.insert(3, list('  #D#C#B#A#'))
 amphipods = ('A', 'B', 'C', 'D')
 energy = [1, 10, 100, 1000]
 rooms = (3, 5, 7, 9)
-visited = {freeze(grid): 0}
-q = [(0, 0, grid)]
 
-
-while q:
-    _, total, grid = heappop(q)
-    visited[freeze(grid)] = total
-    #pprint(grid)
-
-    for i, amphipod in enumerate(amphipods):
-        if grid[2][rooms[i]] != amphipod or grid[3][rooms[i]] != amphipod:
-            break
-    else:
-        print(total)
-
-    for y, row in enumerate(grid):
-        for x, space in enumerate(row):
-            if space in ascii_uppercase:
-                for cost, next_grid in moves(grid, x, y):
-                    if freeze(next_grid) not in visited or total + cost < visited[freeze(next_grid)]:
-                        heappush(q, (-inplace(next_grid), total + cost, next_grid))
+print('Part 1:', search(grid))
+print('Part 2:', search(second_grid))

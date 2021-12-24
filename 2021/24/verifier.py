c3fe0fd05e06253c81ef33ef47bae04c9b3e17a9
John Pignata <pignataj@amazon.com>
2021-12-24 09:33:24 -0500

    2021/24, verification script

---
+++ 2021/24/verifier.py
@@ -0,0 +1,44 @@
+import sys
+
+
+class ALU(object):
+    def __init__(self, instructions):
+        self.instructions = [instruction.strip() .split()
+                             for instruction in instructions]
+
+    def run(self, model):
+        registers = {'w': 0, 'x': 0, 'y': 0, 'z': 0}
+        model = (int(d) for d in list(str(model)))
+
+        for instruction in instructions:
+            if len(instruction.split()) == 3:
+                operation, a, b = instruction.split()
+
+                if b in ('w', 'x', 'y', 'z'):
+                    b = registers[b]
+                else:
+                    b = int(b)
+            elif len(instruction.split()) == 2:
+                operation, a = instruction.split()
+
+            match operation:
+                case 'inp':
+                    registers[a] = next(model)
+                case 'add':
+                    registers[a] += b
+                case 'mul':
+                    registers[a] *= b
+                case 'div':
+                    registers[a] //= b 
+                case 'mod':
+                    registers[a] %= b
+                case 'eql':
+                    registers[a] = 1 if registers[a] == b else 0
+
+        return registers['z'] == 0
+
+
+instructions = [line.strip() for line in sys.stdin]
+alu = ALU(instructions)
+
+print(alu.run(sys.argv[1]))

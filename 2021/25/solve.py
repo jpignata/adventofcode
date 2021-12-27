b04e84366963333a9d4e926e8c416b30a786a40b
John Pignata <pignataj@amazon.com>
2021-12-26 22:34:26 -0500

    2021/25, solve with sets
    Solving in place as a dict worked but took about 7 seconds as all empty
    cells had to be inspected twice per iteration. Using sets to indicate
    a "sea cucumber", this can be done with only iterating occupied spaces.
    Still about a second.

--- 2021/25/solve.py
+++ 2021/25/solve.py
@@ -1,34 +1,41 @@
 import sys
 from itertools import count
 
-grid = {(x, y): char
-        for y, line in enumerate(sys.stdin)
-        for x, char in enumerate(line.strip())}
-lenx, leny = [max(c) + 1 for c in zip(*grid.keys())]
+east, south = set(), set()
+lenx, leny = 0, 0
+
+for y, line in enumerate(sys.stdin):
+    leny = max(leny, y + 1)
+
+    for x, char in enumerate(line.strip()):
+        lenx = max(lenx, x + 1)
+
+        if char == '>':
+            east.add((x, y))
+        elif char == 'v':
+            south.add((x, y))
 
 for step in count(1):
-    moved = False
+    next_east, next_south = set(), set()
 
-    for (x, y) in grid:
+    for (x, y) in east:
         adjacent = ((x + 1) % lenx, y)
 
-        if grid[(x, y)] == '>' and grid[adjacent] == '.':
-            grid[(x, y)] = '..'
-            grid[adjacent] = '>>'
-            moved = True
+        if adjacent not in east and adjacent not in south:
+            next_east.add(adjacent)
+        else:
+            next_east.add((x, y))
 
-    for (x, y) in grid:
+    for (x, y) in south:
         adjacent = (x, (y + 1) % leny)
 
-        if grid[(x, y)] == 'v' and grid[adjacent] in ('.', '..'):
-            grid[(x, y)] = '...'
-            grid[adjacent] = 'vv'
-            moved = True
+        if adjacent not in next_east and adjacent not in south:
+            next_south.add(adjacent)
+        else:
+            next_south.add((x, y))
 
-    if not moved:
+    if next_east == east and next_south == south:
         print('Part 1:', step)
         break
 
-    for (x, y), char in grid.items():
-        if len(char) > 1:
-            grid[(x, y)] = char[0]
+    east, south = next_east, next_south

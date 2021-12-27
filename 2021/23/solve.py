0a3ba79fe07c81837916b5bea2f3c48a480c1da6
John Pignata <pignataj@amazon.com>
2021-12-26 20:47:55 -0500

    2021/23, give up on A*
    Can't find a heuristic that returns the exact right answer. I assume
    this is because of the puzzle constraints. Djistrka's finds it quickly
    enough (20 seconds), though with the heuristic I get an answer back in
    half a second. Problem is, part 1 is wrong.

--- 2021/23/solve.py
+++ 2021/23/solve.py
@@ -3,7 +3,6 @@ from collections import defaultdict
 from copy import deepcopy
 from heapq import heappush, heappop
 from math import inf
-from string import ascii_uppercase
 
 
 def freeze(grid):
@@ -14,32 +13,13 @@ def distance(p1, p2):
     return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
 
 
-def heuristic(grid):
-    score = 0
-
-    for y, row in enumerate(grid):
-        for x, space in enumerate(row):
-            if space in ascii_uppercase:
-                room = rooms[ord(space) - 65]
-                spaces = [row[room] for row in grid
-                          if row[room] in ascii_uppercase]
-
-                if y == 1 and x != room:
-                    score += distance((x, y), (room, 1))
-                elif y != 1 and (x != room or len(set(spaces)) > 1):
-                    score += distance((x, y), (room, 1)) + 1
-
-    return score
-
-
 def moves(grid, x, y):
     valid = (1, 2, 4, 6, 8, 10, 11)
     cost = energy[ord(grid[y][x]) - 65]
-    destination = rooms[ord(grid[y][x]) - 65]
+    destx = rooms[ord(grid[y][x]) - 65]
+    spaces = [row[destx] for row in grid if row[destx] in amphipods]
 
-    if x == destination and y == 3:
-        return
-    elif x == destination and y == 2 and grid[3][x] == grid[y][x]:
+    if x == destx and all(space == grid[y][x] for space in spaces):
         return
 
     if y == 1:
@@ -47,9 +27,9 @@ def moves(grid, x, y):
             nx = x
 
             while grid[y][nx + dx] == '.':
-                if nx + dx == destination:
+                if nx + dx == destx:
                     room = [grid[ny][nx + dx] for ny in range(y, len(grid))
-                            if grid[ny][nx + dx] in ascii_uppercase]
+                            if grid[ny][nx + dx] in amphipods]
 
                     if all(space == grid[y][x] for space in room) or not room:
                         ny = len(grid) - len(room) - 2
@@ -85,39 +65,36 @@ def search(grid):
     visited = defaultdict(lambda: inf)
     visited[freeze(grid)] = 0
     q = [(0, 0, grid)]
-    found = []
 
     while q:
         _, total, grid = heappop(q)
-        visited[freeze(grid)] = total
 
         for i, amphipod in enumerate(amphipods):
             if grid[2][rooms[i]] != amphipod or grid[3][rooms[i]] != amphipod:
                 break
         else:
-            found.append(total)
-
-            if len(found) == 5:
-                return min(found)
+            return total
 
         for y, row in enumerate(grid):
             for x, space in enumerate(row):
-                if space in ascii_uppercase:
-                    for cost, next_grid in moves(grid, x, y):
-                        if total + cost < visited[freeze(next_grid)]:
-                            score = heuristic(next_grid)
-                            heappush(q, (score, total + cost, next_grid))
+                if space not in amphipods:
+                    continue
 
-    return min(found)
+                for cost, next_grid in moves(grid, x, y):
+                    grid_key = freeze(next_grid)
+
+                    if (next_cost := total + cost) < visited[grid_key]:
+                        visited[grid_key] = next_cost
+                        heappush(q, (next_cost, next_cost, next_grid))
 
 
-grid = [[c for c in line.rstrip()] for line in sys.stdin]
-second_grid = deepcopy(grid)
-second_grid.insert(3, list('  #D#B#A#C#'))
-second_grid.insert(3, list('  #D#C#B#A#'))
 amphipods = ('A', 'B', 'C', 'D')
-energy = [1, 10, 100, 1000]
+energy = (1, 10, 100, 1000)
 rooms = (3, 5, 7, 9)
+grid = [[c for c in line.rstrip()] for line in sys.stdin]
+grid2 = deepcopy(grid)
+grid2.insert(3, list('  #D#B#A#C#'))
+grid2.insert(3, list('  #D#C#B#A#'))
 
 print('Part 1:', search(grid))
-print('Part 2:', search(second_grid))
+print('Part 2:', search(grid2))

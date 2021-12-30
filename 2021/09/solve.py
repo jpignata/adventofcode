1d4f27b1b26ed3cca34b39e4ba927269d2435c3d
John Pignata <pignataj@amazon.com>
2021-12-30 13:00:01 -0500

    2021/09, simplify
    - Use heappushpop instead of a while loop
    - Use guards in the comprehension rather than separate checks
    - Rename lowest to risk to match puzzle description

--- 2021/09/solve.py
+++ 2021/09/solve.py
@@ -1,41 +1,38 @@
 import sys
-from heapq import heappush, heappop
+from heapq import heappush, heappushpop
 from math import prod
-
-grid = [[int(point) for point in line.strip()] for line in sys.stdin]
-lowest = 0
-largest = []
-deltas = ((1, 0), (0, 1), (-1, 0), (0, -1))
 
 
 def dfs(x, y, visited):
-    if ((x, y) in visited):
-        return 0
-
     visited.add((x, y))
 
-    if (grid[y][x] == 9):
-        return 0
-
     return 1 + sum([dfs(nx, ny, visited) for dx, dy in deltas
-                    if 0 <= (nx := x+dx) < len(grid[0])
-                    if 0 <= (ny := y+dy) < len(grid)])
+                    if 0 <= (nx := x+dx) < length
+                    if 0 <= (ny := y+dy) < length
+                    if (nx, ny) not in visited
+                    if grid[ny][nx] != 9])
 
+
+deltas = ((1, 0), (0, 1), (-1, 0), (0, -1))
+grid = [[int(point) for point in line.strip()] for line in sys.stdin]
+length = len(grid)
+risk = 0
+largest = []
 
 for y, row in enumerate(grid):
     for x, cell in enumerate(row):
         neighbors = [grid[ny][nx] for dx, dy in deltas
-                     if 0 <= (nx := x+dx) < len(grid[0])
-                     if 0 <= (ny := y+dy) < len(grid)]
+                     if 0 <= (nx := x+dx) < length
+                     if 0 <= (ny := y+dy) < length]
 
-        if min(neighbors) > cell:
+        if cell < min(neighbors):
+            risk += cell + 1
             size = dfs(x, y, set())
-            lowest += cell + 1
 
-            heappush(largest, size)
+            if len(largest) < 3:
+                heappush(largest, size)
+            else:
+                heappushpop(largest, size)
 
-            while len(largest) > 3:
-                heappop(largest)
-
-print('Part 1:', lowest)
+print('Part 1:', risk)
 print('Part 2:', prod(largest))

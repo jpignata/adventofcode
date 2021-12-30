937677ba848d8b585f44959d297bd8443b60c9df
John Pignata <john@pignata.com>
2021-12-30 17:25:23 -0500

    2021/03, minor clean-up

--- 2021/03/solve.py
+++ 2021/03/solve.py
@@ -1,18 +1,11 @@
 import sys
 from collections import Counter
 
-LEAST_COMMON = -1
-MOST_COMMON = 0
-
-
-def to_decimal(bits):
-    return int(''.join(bits), 2)
-
 
 def find(numbers, *, strategy, tiebreaker):
     for position in range(len(numbers[0])):
-        cols = list(zip(*numbers))
-        counts = Counter(cols[position]).most_common()
+        positions = list(zip(*numbers))
+        counts = Counter(positions[position]).most_common()
 
         if len(counts) == 1:
             target = counts[0][0]
@@ -27,13 +20,15 @@ def find(numbers, *, strategy, tiebreaker):
             return numbers[0]
 
 
-numbers = [list(line.strip()) for line in sys.stdin]
-cols = list(zip(*numbers))
+def to_decimal(bits):
+    return int(''.join(bits), 2)
 
-gamma = ['1' if bits.count('1') > len(cols[0]) / 2 else '0' for bits in cols]
-epsilon = ['0' if bit == '1' else '1' for bit in gamma]
-generator = find(numbers, strategy=MOST_COMMON, tiebreaker='1')
-scrubber = find(numbers, strategy=LEAST_COMMON, tiebreaker='0')
+
+numbers = [list(line.strip()) for line in sys.stdin]
+gamma = [str(int(pos.count('1') > len(numbers) / 2)) for pos in zip(*numbers)]
+epsilon = [str(int(bit == '0')) for bit in gamma]
+generator = find(numbers, strategy=0, tiebreaker='1')
+scrubber = find(numbers, strategy=1, tiebreaker='0')
 
 print('Part 1:', to_decimal(gamma) * to_decimal(epsilon))
 print('Part 2:', to_decimal(generator) * to_decimal(scrubber))

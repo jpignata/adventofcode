366d1d6edbea6e4b780606caa88a9951334919c8
John Pignata <pignataj@amazon.com>
2022-07-21 20:36:52 -0400

    2021/16, reformat with black

--- 2021/16/solve.py
+++ 2021/16/solve.py
@@ -1,44 +1,52 @@
 import sys
-from operator import lt, gt, eq
+
 from io import StringIO
 from math import prod
+from operator import lt, gt, eq
 
 
 def parse(reader):
+    literals = []
+    versions = [int(reader.read(3), 2)]
+    packet_type = int(reader.read(3), 2)
+
     def decode():
         version, literal = parse(reader)
         versions.extend(version)
         literals.append(literal)
 
-    literals = []
-    versions = [int(reader.read(3), 2)]
-    packet_type = int(reader.read(3), 2)
-
     if packet_type == 4:
-        literal = ''
+        literal = ""
 
-        while (segment := reader.read(5)):
+        while segment := reader.read(5):
             literal += segment[1:]
 
-            if int(segment[0]) == 0:
-                break
+            if not int(segment[0]):
+                return versions, int(literal, 2)
 
-        return versions, int(literal, 2)
-    elif (length_type := int(reader.read(1))) == 0:
+    if int(reader.read(1)):
+        for _ in range(int(reader.read(11), 2)):
+            decode()
+    else:
         read_to = int(reader.read(15), 2) + reader.tell()
 
         while reader.tell() != read_to:
-            decode()
-    elif length_type == 1:
-        for _ in range(int(reader.read(11), 2)):
             decode()
 
     return versions, operations[packet_type](literals)
 
 
-operations = {0: sum, 1: prod, 2: min, 3: max, 5: lambda x: gt(*x), 6: lambda x: lt(*x), 7: lambda x: eq(*x)}
-bits = ''.join(bin(int(c, 16))[2:].zfill(4) for c in sys.stdin.read().strip())
+operations = {
+    0: sum,
+    1: prod,
+    2: min,
+    3: max,
+    5: lambda x: gt(*x),
+    6: lambda x: lt(*x),
+    7: lambda x: eq(*x),
+}
+bits = "".join(bin(int(c, 16))[2:].zfill(4) for c in sys.stdin.read().strip())
 versions, result = parse(StringIO(bits))
 
-print('Part 1:', sum(versions))
-print('Part 2:', result)
+print("Part 1:", sum(versions))
+print("Part 2:", result)

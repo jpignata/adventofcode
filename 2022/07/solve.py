f1697c6154e2ed14bd23a4d1c619e0b95e832288
John Pignata <john@pignata.com>
2022-12-07 10:52:06 -0500

    2022/07, simplify
    I thought part 2 would be harder. 🤷

--- 2022/07/solve.py
+++ 2022/07/solve.py
@@ -1,71 +1,26 @@
 import sys
-from dataclasses import dataclass, field
-from math import inf
-from typing import Dict
+from collections import defaultdict
 
 
-@dataclass
-class Directory:
-    size: int = 0
-    parent: "Directory" = None
-    children: Dict[str, "Directory"] = field(default_factory=dict)
+dirs = defaultdict(int)
+current = ["root"]
 
+for line in sys.stdin:
+    match line.strip().split():
+        case ["$", "cd", "/"]:
+            current = ["root"]
+        case ["$", "cd", ".."]:
+            current.pop()
+        case ["$", "cd", name]:
+            current.append(name)
+        case ["$", "ls"] | ["dir", _]:
+            continue
+        case [size, _]:
+            for segment in current:
+                dirs[segment] += int(size)
 
-def solve():
-    root = build(Directory())
+total = sum(size for size in dirs.values() if size <= 100_000)
+smallest = min(size for size in dirs.values() if size >= dirs["root"] - 40_000_000)
 
-    print("Part 1:", part1(root))
-    print("Part 2:", part2(root, root.size - 40_000_000))
-
-
-def build(root):
-    current = root
-
-    for line in sys.stdin:
-        match line.strip().split():
-            case ["$", "cd", "/"]:
-                current = root
-            case ["$", "cd", ".."]:
-                current = current.parent
-            case ["$", "cd", name]:
-                current = current.children[name]
-            case ["$", "ls"]:
-                continue
-            case ["dir", name]:
-                current.children[name] = Directory(parent=current)
-            case [size, _]:
-                node = current
-
-                while node:
-                    node.size += int(size)
-                    node = node.parent
-
-    return root
-
-
-def part1(root):
-    total = 0
-
-    for node in root.children.values():
-        total += part1(node)
-
-        if node.size <= 100_000:
-            total += node.size
-
-    return total
-
-
-def part2(root, min_size):
-    smallest = inf
-
-    for node in root.children.values():
-        smallest = min(smallest, part2(node, min_size))
-
-        if node.size >= min_size:
-            smallest = min(smallest, node.size)
-
-    return smallest
-
-
-if __name__ == "__main__":
-    solve()
+print("Part 1:", total)
+print("Part 2:", smallest)

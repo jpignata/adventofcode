9dcf6349b5ad8c7ae7f8b5ae8d1a85366db8fdcd
John Pignata <john@pignata.com>
2022-12-14 13:43:49 -0500

    2022/14, minor clean-up
    - Use zip instead of pairwise
    - Remove unnecessary break and eliminate "while True" smell
    - Massage names

--- 2022/14/solve.py
+++ 2022/14/solve.py
@@ -1,40 +1,40 @@
 import sys
 from collections import defaultdict
-from itertools import pairwise
 from re import findall
 
 
 def solve():
-    grid = defaultdict(lambda: ".")
     paths = [
         [(int(x), int(y)) for x, y in findall(r"(\d+),(\d+)", line)]
         for line in sys.stdin
     ]
+    grid = defaultdict(lambda: ".")
 
     for path in paths:
-        for points in pairwise(path):
+        for points in zip(path, path[1:]):
             (sx, sy), (ex, ey) = sorted(points)
 
             for y in range(sy, ey + 1):
                 for x in range(sx, ex + 1):
-                    grid[(x, y)] = "#"
+                    grid[x, y] = "#"
 
     print("Part 1:", simulate(grid.copy()))
     print("Part 2:", simulate(grid.copy(), floor=True))
 
 
 def simulate(grid, *, floor=False):
-    settled = prev = 0
-    edge = max(point[1] for point in grid.keys()) + 2
+    settled = 0
+    prev = None
+    bottom = max(point[1] for point in grid) + 2
 
     if floor:
         for x in range(0, 1000):
-            grid[(x, edge)] = "#"
+            grid[x, bottom] = "#"
 
-    while True:
+    while prev != (prev := settled):
         cx, cy = 500, 0
 
-        while grid[cx, cy] != "o" and cy < edge:
+        while grid[cx, cy] != "o" and cy < bottom:
             if grid[cx, cy + 1] == ".":
                 cy += 1
             elif grid[cx - 1, cy + 1] == ".":
@@ -46,12 +46,8 @@ def simulate(grid, *, floor=False):
             else:
                 settled += 1
                 grid[cx, cy] = "o"
-                break
 
-        if settled == prev:
-            return settled
-
-        prev = settled
+    return settled
 
 
 if __name__ == "__main__":

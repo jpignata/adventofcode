c79076101c66ecb33285b126a534d33e170a9b29
John Pignata <john@pignata.com>
2022-12-18 15:12:32 -0500

    2022/17, minor clean-up

--- 2022/17/solve.py
+++ 2022/17/solve.py
@@ -4,71 +4,63 @@ from itertools import cycle
 
 def solve():
     jets = list(sys.stdin.readline().strip())
+    rocks = [
+        ((0, 0), (1, 0), (2, 0), (3, 0)),
+        ((1, 0), (0, 1), (1, 1), (2, 1), (1, 2)),
+        ((0, 0), (1, 0), (2, 0), (2, 1), (2, 2)),
+        ((0, 0), (0, 1), (0, 2), (0, 3)),
+        ((0, 0), (1, 0), (0, 1), (1, 1)),
+    ]
 
-    print("Part 1:", simulate(jets, 7, 2022))
-    print("Part 2:", simulate(jets, 7, 1000000000000, detect=True))
+    print("Part 1:", simulate(jets, rocks, 2022))
+    print("Part 2:", simulate(jets, rocks, 1000000000000))
 
 
-def simulate(jets, width, drops, *, detect=False):
+def simulate(jets, rocks, drops):
     def move(rock, dx, dy):
-        new_rock = tuple((x + dx, y + dy) for x, y in rock)
+        next_rock = tuple((x + dx, y + dy) for x, y in rock)
 
-        if any(x < 0 or y < 0 or x >= width for x, y in new_rock):
+        if any(x < 0 or y < 0 or x >= 7 for x, y in next_rock):
             return rock
 
-        if any(grid.get((x, y), 0) for x, y in new_rock):
+        if any(grid.get((x, y), 0) for x, y in next_rock):
             return rock
 
-        return new_rock
+        return next_rock
 
     grid = {}
     seen = {}
     jets = cycle(enumerate(jets))
-    rocks = cycle(
-        enumerate(
-            [
-                ((0, 0), (1, 0), (2, 0), (3, 0)),
-                ((1, 0), (0, 1), (1, 1), (2, 1), (1, 2)),
-                ((0, 0), (1, 0), (2, 0), (2, 1), (2, 2)),
-                ((0, 0), (0, 1), (0, 2), (0, 3)),
-                ((0, 0), (1, 0), (0, 1), (1, 1)),
-            ]
-        )
-    )
+    rocks = cycle(enumerate(rocks))
 
     for drop in range(drops + 1):
-        rock_index, rock = next(rocks)
         height = max((y for _, y in grid), default=-1)
+        rock_index, rock = next(rocks)
         rock = tuple((x + 2, y + height + 4) for x, y in rock)
 
         while True:
             jet_index, jet = next(jets)
-            dx = 1 if jet == ">" else -1
+            rock = move(rock, 1 if jet == ">" else -1, 0)
+            next_rock = move(rock, 0, -1)
 
-            rock = move(rock, dx, 0)
-            new_rock = move(rock, 0, -1)
+            if next_rock == rock:
+                for x, y in rock:
+                    grid[x, y] = 1
 
-            if new_rock == rock:
                 break
 
-            rock = new_rock
+            rock = next_rock
 
-            if detect:
-                height = max((y for _, y in grid), default=0)
+        if (rock_index, jet_index) in seen:
+            prev_drop, prev_height = seen[rock_index, jet_index]
+            remaining = drops - drop
+            period = drop - prev_drop
+            offset = height - prev_height
 
-                if (rock_index, jet_index) in seen:
-                    seendrop, seenheight = seen[rock_index, jet_index]
+            if remaining % period == 0:
+                return (remaining // period) * offset + height + 1
 
-                    if (drops - drop) % (drop - seendrop) == 0:
-                        remaining = (drops - drop) // (drop - seendrop)
-                        offset = height - seenheight
-
-                        return remaining * offset + height + 1
-
-                seen[rock_index, jet_index] = drop, height
-
-        for x, y in rock:
-            grid[x, y] = 1
+        seen[rock_index, jet_index] = drop, height
 
     return height + 1
 

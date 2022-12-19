7195fbf6d629c9a97bf9243a4b61c3e5063567c4
John Pignata <john@pignata.com>
2022-12-18 21:30:56 -0500

    2022/06, make pylint happy

--- 2022/06/solve.py
+++ 2022/06/solve.py
@@ -1,13 +1,20 @@
 import sys
 
 
+def solve():
+    buffer = sys.stdin.readline().strip()
+
+    print("Part 1:", marker(buffer, 4))
+    print("Part 2:", marker(buffer, 14))
+
+
 def marker(buffer, window):
     for i in range(len(buffer) - window):
         if len(set(buffer[i : i + window])) == window:
             return i + window
 
+    return -1
 
-buffer = sys.stdin.readline().strip()
 
-print("Part 1:", marker(buffer, 4))
-print("Part 2:", marker(buffer, 14))
+if __name__ == "__main__":
+    solve()

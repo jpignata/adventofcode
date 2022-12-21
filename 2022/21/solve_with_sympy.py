565e13b8221574dc14341c5e2ce17701b2e74a90
John Pignata <john@pignata.com>
2022-12-21 14:01:54 -0500

    2022/21, solve with sympy
    A better approach to this problem is using an equation solver, which I
    wasn't familiar with. This solution also has the benefit of passing for
    both the sample input and my input which the binary search doesn't as I
    didn't properly identify the boundary checks. I typically try to solve
    these without imports, but this was a cool thing to learn, so including
    it here separately.

---
+++ 2022/21/solve_with_sympy.py
@@ -0,0 +1,41 @@
+import sys
+from re import split
+
+from sympy import simplify
+from sympy import solve as sympy_solve
+from sympy.abc import x
+
+
+def solve():
+    monkeys = {}
+
+    for line in sys.stdin:
+        match split(r":? ", line.strip()):
+            case [monkey, number]:
+                monkeys[monkey] = int(number)
+            case [monkey, arg1, operator, arg2]:
+                monkeys[monkey] = [operator, arg1, arg2]
+
+    def build(monkey):
+        match monkey:
+            case "x":
+                return x
+            case int(monkey):
+                return monkey
+            case [operator, *names]:
+                left, right = [build(monkeys[name]) for name in names]
+
+                return f"({left} {operator} {right})"
+
+    def find():
+        monkeys["humn"] = "x"
+        left, right = [build(monkeys[name]) for name in monkeys["root"][1:]]
+
+        return sympy_solve(f"Eq({left}, {right})", x)[0]
+
+    print("Part 1:", simplify(build(monkeys["root"])))
+    print("Part 2:", find())
+
+
+if __name__ == "__main__":
+    solve()

8817f4fa346d2e2e41b81d45975b3ac3a9ead2e0
John Pignata <john@pignata.com>
2023-12-01 09:38:13 -0500

    2023/01, minor cleanup
    Use isdigit() and iterate through the characters rather than every
    possible digit.

--- 2023/01/solve.py
+++ 2023/01/solve.py
@@ -1,5 +1,4 @@
 import sys
-from string import digits
 from re import finditer
 
 part1 = part2 = 0
@@ -17,19 +16,14 @@ words = [
 
 for line in sys.stdin:
     digit_values = [
-        (match.start(), int(digit))
-        for digit in digits
-        for match in finditer(digit, line)
+        (i, int(char)) for i, char in enumerate(line) if char.isdigit()
     ]
     word_values = [
         (match.start(), j + 1)
         for j, word in enumerate(words)
         for match in finditer(word, line)
     ]
-    values = digit_values + word_values
-
-    digit_values.sort()
-    values.sort()
+    values = sorted(digit_values + word_values)
 
     part1 += digit_values[0][1] * 10 + digit_values[-1][1]
     part2 += values[0][1] * 10 + values[-1][1]

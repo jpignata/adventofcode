b7365cdb482e715c25a2124f4c8eb41273199407
John Pignata <john@pignata.com>
2023-12-09 07:03:39 -0500

    2023/09, simplify

--- 2023/09/solve.py
+++ 2023/09/solve.py
@@ -1,23 +1,17 @@
 import sys
 
-part1 = part2 = 0
 
-for line in sys.stdin:
-    numbers = [int(number) for number in line.strip().split()]
-    heads = [numbers[0]]
-    tails = [numbers[-1]]
-    prev = 0
+def extrapolate(nums):
+    tails = []
 
-    while any(numbers):
-        numbers = [num2 - num1 for num1, num2 in zip(numbers, numbers[1:])]
-        heads.append(numbers[0])
-        tails.append(numbers[-1])
+    while any(nums):
+        tails.append(nums[-1])
+        nums = [num2 - num1 for num1, num2 in zip(nums, nums[1:])]
 
-    for num in heads[::-1]:
-        prev = num - prev
+    return sum(tails)
 
-    part1 += sum(tails)
-    part2 += prev
 
-print("Part 1:", part1)
-print("Part 2:", part2)
+sequences = [[int(num) for num in line.split()] for line in sys.stdin]
+
+print("Part 1:", sum(extrapolate(nums) for nums in sequences))
+print("Part 2:", sum(extrapolate(nums[::-1]) for nums in sequences))

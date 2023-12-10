35f35393f9a5e8ffaaf1fb9a4c9ff21698691f27
John Pignata <john@pignata.com>
2023-12-10 15:53:40 -0500

    2023/10, give up on flood fill, count crosses

--- 2023/10/solve.py
+++ 2023/10/solve.py
@@ -1,5 +1,4 @@
 import sys
-from collections import deque
 
 grid = {}
 moves = {
@@ -10,77 +9,37 @@ moves = {
     "7": ((0, 1), (-1, 0)),
     "F": ((0, 1), (1, 0)),
 }
-start = (-1, -1)
-dists = {}
-q = deque([])
 
 for y, line in enumerate(sys.stdin):
     for x, char in enumerate(line.strip()):
         grid[(x, y)] = char
 
         if char == "S":
+            grid[(x, y)] = "7"
             start = (x, y)
-            dists[(x, y)] = 0
 
-for dx, dy in ((0, 1), (0, -1), (1, 0), (-1, 0)):
-    nx, ny = start[0] + dx, start[1] + dy
+s = [start]
+visited = set()
+steps = inside = 0
 
-    if grid[(nx, ny)] != ".":
-        for ddx, ddy in moves[grid[(nx, ny)]]:
-            nnx, nny = nx + ddx, ny + ddy
-
-            if nnx == start[0] and nny == start[1]:
-                q.append((start[0] + dx, start[1] + dy, 1))
-
-while q:
-    x, y, steps = q.popleft()
-    dists[(x, y)] = steps
+while s:
+    x, y = s.pop()
+    steps += 1
+    visited.add((x, y))
 
     for dx, dy in moves[grid[(x, y)]]:
         nx, ny = x + dx, y + dy
 
-        if grid[(nx, ny)] != "." and (nx, ny) not in dists:
-            q.append((nx, ny, steps + 1))
+        if grid[(nx, ny)] != "." and (nx, ny) not in visited:
+            s.append((nx, ny))
 
+for x, y in set(grid) - visited:
+    crosses = [
+        (x1, y)
+        for x1 in range(x)
+        if (x1, y) in visited and grid[(x1, y)] in "|JL"
+    ]
+    inside += len(crosses) % 2
 
-empty = {(x, y) for (x, y), char in grid.items() if (x, y) not in dists}
-outside = set()
-maxx, maxy = max(grid)
-
-for x, y in empty:
-    visited = set()
-    cells = [(x, y)]
-    s = [(x, y)]
-
-    if (x, y) in outside:
-        continue
-
-    while s:
-        nx, ny = s.pop()
-
-        if (nx, ny) in visited:
-            continue
-
-        visited.add((nx, ny))
-
-        if (nx, ny) not in grid:
-            for cell in cells:
-                outside.add(cell)
-        else:
-            char = grid[(nx, ny)]
-
-            if char == ".":
-                cells.append((nx, ny))
-
-                for dx, dy in ((0, 1), (0, -1), (1, 0), (-1, 0)):
-                    s.append((nx + dx, ny + dy))
-
-            elif char in ("-", "|", "J", "F", "L", "7"):
-                for dx, dy in ((0, 1), (0, -1), (1, 0), (-1, 0)):
-                    if (dx, dy) not in moves[char]:
-                        if (nx, ny) not in dists:
-                            cells.append((nx, ny))
-                            s.append((nx + dx, ny + dy))
-
-print("Part 1:", max(dists.values()))
-print("Part 2:", len(empty - outside))
+print("Part 1:", (steps - 1) // 2)
+print("Part 2:", inside)

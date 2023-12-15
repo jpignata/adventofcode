f35012a095edc0213ad8e067dce9c7a7229a2e9f
John Pignata <john@pignata.com>
2023-12-15 18:10:11 -0500

    2023/15

---
+++ 2023/15/solve.py
@@ -0,0 +1,41 @@
+import sys
+from re import split
+
+
+def _hash(seq):
+    val = 0
+
+    for char in seq:
+        val += ord(char)
+        val *= 17
+        val %= 256
+
+    return val
+
+
+sequences = sys.stdin.read().strip().split(",")
+buckets = [[] for _ in range(256)]
+
+for seq in sequences:
+    key, value = split(r"[-=]", seq)
+    bucket_id = _hash(key)
+    bucket = buckets[bucket_id]
+
+    if value:
+        for slot in bucket:
+            if slot[0] == key:
+                slot[1] = value
+                break
+        else:
+            bucket.append([key, value])
+    else:
+        buckets[bucket_id] = [slot for slot in bucket if slot[0] != key]
+
+power = sum(
+    (i + 1) * (j + 1) * int(value)
+    for i, bucket in enumerate(buckets)
+    for j, (_, value) in enumerate(bucket)
+)
+
+print("Part 1:", sum(_hash(seq) for seq in sequences))
+print("Part 2:", power)

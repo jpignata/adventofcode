1b29e594eb1e179ddb0142ff76a6793c43c07995
John Pignata <john@pignata.com>
2023-12-16 08:53:22 -0500

    2023/16, minor cleanup, fix bug

--- 2023/16/solve.py
+++ 2023/16/solve.py
@@ -7,21 +7,18 @@ def activate(x, y, dir):
     visited = set()
 
     def enqueue(x, y, dir):
-        nx, ny = x + dir[0], y + dir[1]
-
-        if 0 <= nx < maxx and 0 <= ny < maxy:
+        if 0 <= (nx := x + dir[0]) < maxx and 0 <= (ny := y + dir[1]) < maxy:
             if (nx, ny, dir) not in visited:
                 q.append((nx, ny, dir))
 
     while q:
         x, y, dir = q.popleft()
-        cell = grid[y][x]
 
-        if cell == "/":
+        if grid[y][x] == "/":
             enqueue(x, y, (dir[1] * -1, dir[0] * -1))
-        elif cell == "\\":
+        elif grid[y][x] == "\\":
             enqueue(x, y, (dir[1], dir[0]))
-        elif (cell == "|" and dir[0]) or (cell == "-" and dir[1]):
+        elif (grid[y][x] == "|" and dir[0]) or (grid[y][x] == "-" and dir[1]):
             enqueue(x, y, (dir[1], dir[0]))
             enqueue(x, y, (dir[1] * -1, dir[0] * -1))
         else:
@@ -32,12 +29,12 @@ def activate(x, y, dir):
     return len({(x, y) for x, y, _ in visited})
 
 
-grid = [list(row) for row in sys.stdin.read().split("\n")]
+grid = [list(row.strip()) for row in sys.stdin.readlines()]
 maxx, maxy = len(grid[0]), len(grid)
 start = [(0, y, (1, 0)) for y in range(maxy)]
 start += [(maxx - 1, y, (-1, 0)) for y in range(maxy)]
 start += [(x, 0, (0, 1)) for x in range(maxx)]
-start += [(x, maxy - 1, (1, 0)) for x in range(maxx)]
+start += [(x, maxy - 1, (0, -1)) for x in range(maxx)]
 
 print("Part 1:", activate(0, 0, (1, 0)))
 print("Part 2:", max(activate(x, y, dir) for x, y, dir in start))

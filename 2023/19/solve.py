9a16e3feab40a740513f6dfce7ccdfd296e4a3ae
John Pignata <john@pignata.com>
2023-12-19 13:57:49 -0500

    2023/19, clean up this hot mess a little

--- 2023/19/solve.py
+++ 2023/19/solve.py
@@ -1,27 +1,29 @@
-# 132755669099550 - too high
-
 import sys
-from re import findall, split
-from operator import gt, lt
+from collections import defaultdict
 from math import prod
+from operator import gt, lt
+from re import findall, split
 
-workflows = {}
+workflows = defaultdict(list)
 ratings = []
-ops = {"<": lt, ">": gt}
 
 for line in sys.stdin:
     if line.startswith("{"):
-        n = [int(num) for num in findall(r"\d+", line)]
-        ratings.append({"x": n[0], "m": n[1], "a": n[2], "s": n[3]})
+        ratings.append(
+            {
+                letter: int(number)
+                for letter, number in zip("xmas", findall(r"\d+", line))
+            }
+        )
     elif line.strip():
         name, rules = line[:-2].split("{")
         workflows[name] = []
 
         for rule in rules.split(","):
             if ":" in rule:
-                condition, destination = rule.split(":")
+                condition, dest = rule.split(":")
                 var, op, value = split(r"([<>])", condition)
-                workflows[name].append((var, op, int(value), destination))
+                workflows[name].append((var, op, int(value), dest))
             else:
                 workflows[name].append((None, None, None, rule))
 
@@ -34,14 +36,14 @@ def run(ratings):
 
         while current not in "AR":
             for rule in workflows[current]:
-                var, op, value, destination = rule
+                var, op, value, dest = rule
 
                 if var:
-                    if ops[op](rating[var], value):
-                        current = destination
+                    if {"<": lt, ">": gt}[op](rating[var], value):
+                        current = dest
                         break
                 else:
-                    current = destination
+                    current = dest
 
         if current == "A":
             total += sum(rating.values())
@@ -49,9 +51,9 @@ def run(ratings):
     return total
 
 
-def find(ranges):
-    s = [("in", ranges)]
+def find(start=(1, 4000)):
     total = 0
+    s = [("in", {"x": start, "m": start, "a": start, "s": start})]
 
     while s:
         current, ranges = s.pop()
@@ -64,52 +66,25 @@ def find(ranges):
             continue
 
         for rule in workflows[current]:
-            var, op, value, destination = rule
+            var, op, value, dest = rule
 
             if op is None:
-                s.append((destination, ranges))
+                s.append((dest, ranges))
                 break
 
             r = ranges[var]
 
             if op == "<" and r[0] < value:
-                s.append(
-                    (
-                        destination,
-                        {**ranges, var: (r[0], min(r[1], value) - 1)},
-                    )
-                )
-                s.append(
-                    (
-                        current,
-                        {**ranges, var: (max(r[0], value), r[1])},
-                    )
-                )
+                s.append((dest, {**ranges, var: (r[0], min(r[1], value) - 1)}))
+                s.append((current, {**ranges, var: (max(r[0], value), r[1])}))
                 break
             elif op == ">" and r[1] > value:
-                print(ranges)
-                print(rule)
-                print(value)
-                print()
-                s.append(
-                    (
-                        destination,
-                        {**ranges, var: (max(r[0], value) + 1, r[1])},
-                    )
-                )
-                s.append(
-                    (
-                        current,
-                        {**ranges, var: (r[0], min(r[1], value))},
-                    )
-                )
+                s.append((dest, {**ranges, var: (max(r[0], value) + 1, r[1])}))
+                s.append((current, {**ranges, var: (r[0], min(r[1], value))}))
                 break
 
     return total
 
 
 print("Part 1:", run(ratings))
-print(
-    "Part 2:",
-    find({"x": (1, 4000), "m": (1, 4000), "a": (1, 4000), "s": (1, 4000)}),
-)
+print("Part 2:", find())

32e2f3a5a26b7d0dcf01ec67d65a384ddb4ade67
John Pignata <john@pignata.com>
2023-12-19 20:09:42 -0500

    2023/19, more cleaning up

--- 2023/19/solve.py
+++ 2023/19/solve.py
@@ -4,16 +4,66 @@ from math import prod
 from operator import gt, lt
 from re import findall, split
 
+
+def run(ratings):
+    total = 0
+    ops = {"<": lt, ">": gt, None: lambda a, b: True}
+
+    for rating in ratings:
+        current = "in"
+
+        while current not in "AR":
+            for var, op, value, dest in workflows[current]:
+                if ops[op](rating[var], value):
+                    current = dest
+                    break
+
+        if current == "A":
+            total += sum(rating.values())
+
+    return total
+
+
+def find_in(_range):
+    total = 0
+    s = [("in", {letter: _range for letter in "xmas"})]
+
+    while s:
+        current, ranges = s.pop()
+
+        if current == "A":
+            total += prod(c[1] - c[0] + 1 for c in ranges.values())
+            continue
+
+        if current == "R":
+            continue
+
+        for var, op, value, dest in workflows[current]:
+            start, end = ranges[var]
+
+            if op is None:
+                s.append((dest, ranges))
+            elif op == "<" and start < value:
+                s.append((dest, {**ranges, var: (start, min(end, value) - 1)}))
+                s.append((current, {**ranges, var: (max(start, value), end)}))
+            elif op == ">" and end > value:
+                s.append((dest, {**ranges, var: (max(start, value) + 1, end)}))
+                s.append((current, {**ranges, var: (start, min(end, value))}))
+            else:
+                continue
+
+            break
+
+    return total
+
+
 workflows = defaultdict(list)
 ratings = []
 
 for line in sys.stdin:
     if line.startswith("{"):
         ratings.append(
-            {
-                letter: int(number)
-                for letter, number in zip("xmas", findall(r"\d+", line))
-            }
+            {var: int(num) for var, num in zip("xmas", findall(r"\d+", line))}
         )
     elif line.strip():
         name, rules = line[:-2].split("{")
@@ -25,66 +75,7 @@ for line in sys.stdin:
                 var, op, value = split(r"([<>])", condition)
                 workflows[name].append((var, op, int(value), dest))
             else:
-                workflows[name].append((None, None, None, rule))
-
-
-def run(ratings):
-    total = 0
-
-    for rating in ratings:
-        current = "in"
-
-        while current not in "AR":
-            for rule in workflows[current]:
-                var, op, value, dest = rule
-
-                if var:
-                    if {"<": lt, ">": gt}[op](rating[var], value):
-                        current = dest
-                        break
-                else:
-                    current = dest
-
-        if current == "A":
-            total += sum(rating.values())
-
-    return total
-
-
-def find(start=(1, 4000)):
-    total = 0
-    s = [("in", {"x": start, "m": start, "a": start, "s": start})]
-
-    while s:
-        current, ranges = s.pop()
-
-        if current == "R":
-            continue
-
-        if current == "A":
-            total += prod(c[1] - c[0] + 1 for c in ranges.values())
-            continue
-
-        for rule in workflows[current]:
-            var, op, value, dest = rule
-
-            if op is None:
-                s.append((dest, ranges))
-                break
-
-            r = ranges[var]
-
-            if op == "<" and r[0] < value:
-                s.append((dest, {**ranges, var: (r[0], min(r[1], value) - 1)}))
-                s.append((current, {**ranges, var: (max(r[0], value), r[1])}))
-                break
-            elif op == ">" and r[1] > value:
-                s.append((dest, {**ranges, var: (max(r[0], value) + 1, r[1])}))
-                s.append((current, {**ranges, var: (r[0], min(r[1], value))}))
-                break
-
-    return total
-
+                workflows[name].append(("x", None, 0, rule))
 
 print("Part 1:", run(ratings))
-print("Part 2:", find())
+print("Part 2:", find_in((1, 4000)))

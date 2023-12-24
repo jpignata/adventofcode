6b500acab946b3ecb172f87ae0b48b897bb9800b
John Pignata <john@pignata.com>
2023-12-24 10:56:30 -0500

    2023/24, clean-up

--- 2023/24/solve.py
+++ 2023/24/solve.py
@@ -1,11 +1,9 @@
 import sys
-from dataclasses import dataclass
 from itertools import combinations
 from re import findall
 from sympy import Eq, solve, Symbol, symbols
 
 
-@dataclass
 class Hailstone:
     def __init__(self, sx, sy, sz, vx, vy, vz):
         self.sx = int(sx)
@@ -14,14 +12,8 @@ class Hailstone:
         self.vx = int(vx)
         self.vy = int(vy)
         self.vz = int(vz)
-
-    @property
-    def slope(self):
-        return self.vy / self.vx if self.vx != 0 else -1
-
-    @property
-    def intercept(self):
-        return self.sy - self.slope * self.sx if self.vx != 0 else self.px
+        self.slope = self.vy / self.vx
+        self.intercept = self.sy - self.slope * self.sx
 
 
 def find(h1, h2, area=(2e14, 4e14)):
@@ -30,29 +22,23 @@ def find(h1, h2, area=(2e14, 4e14)):
 
     ix = (h2.intercept - h1.intercept) / (h1.slope - h2.slope)
     iy = h1.slope * ix + h1.intercept
-    t1 = (ix - h1.sx) / h1.vx if h1.vx != 0 else -1
-    t2 = (ix - h2.sx) / h2.vx if h2.vx != 0 else -1
+    t1 = (ix - h1.sx) / h1.vx
+    t2 = (ix - h2.sx) / h2.vx
 
-    if t1 >= 0 and t2 > 0:
-        if area[0] <= ix <= area[1] and area[0] <= iy <= area[1]:
-            return (ix, iy)
+    if t1 > 0 and t2 > 0:
+        return area[0] <= ix <= area[1] and area[0] <= iy <= area[1]
 
 
 def throw(hailstones):
-    eq = []
     t = []
+    eq = []
     x, y, z, vx, vy, vz = symbols("x y z vx vy vz")
 
     for i, h in enumerate(hailstones):
         t.append(Symbol(f"t{i}"))
-
-        eq.extend(
-            [
-                Eq(h.sx + h.vx * t[-1], x + vx * t[-1]),
-                Eq(h.sy + h.vy * t[-1], y + vy * t[-1]),
-                Eq(h.sz + h.vz * t[-1], z + vz * t[-1]),
-            ]
-        )
+        eq.append(Eq(h.sx + h.vx * t[-1], x + vx * t[-1]))
+        eq.append(Eq(h.sy + h.vy * t[-1], y + vy * t[-1]))
+        eq.append(Eq(h.sz + h.vz * t[-1], z + vz * t[-1]))
 
     result = solve(eq, (x, y, z, vx, vy, vz, *t), dict=True)
 

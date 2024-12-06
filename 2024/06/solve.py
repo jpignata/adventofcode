d009b0c0e832eb9f4e4ba34bf40be9993c9bd0de
John Pignata <john@pignata.com>
2024-12-06 08:38:27 -0800

    2024/06, more cleaning

--- 2024/06/solve.py
+++ 2024/06/solve.py
@@ -3,60 +3,54 @@ from collections import defaultdict
 from itertools import cycle
 
 
-def find(x, y, obstacles, boundary):
-    visited = defaultdict(int)
-    path = set()
+def traverse(x, y, obstructions, edge):
+    visited = defaultdict(set)
     dirs = cycle([(0, -1), (1, 0), (0, 1), (-1, 0)])
-    dir_ = next(dirs)
+    dx, dy = next(dirs)
 
     while True:
-        if visited[(x, y, dir_)]:
-            return set(), True
+        if (dx, dy) in visited[(x, y)]:
+            return {}, True
 
-        visited[(x, y, dir_)] += 1
-        path.add((x, y))
+        visited[(x, y)].add((dx, dy))
 
         while True:
-            nx, ny = x + dir_[0], y + dir_[1]
+            nx, ny = x + dx, y + dy
 
-            if nx < 0 or nx > boundary or ny < 0 or ny > boundary:
-                return path, False
+            if nx < 0 or nx > edge or ny < 0 or ny > edge:
+                return visited, False
 
-            if (nx, ny) not in obstacles:
+            if (nx, ny) not in obstructions:
                 break
 
-            dir_ = next(dirs)
+            dx, dy = next(dirs)
 
         x, y = nx, ny
 
 
-def search(sx, sy, path, obstacles, boundary):
-    return sum(
-        has_cycle
-        for _, has_cycle in (
-            find(sx, sy, obstacles | {(x, y)}, boundary) for x, y in path
-        )
-    )
-
-
 def main():
-    obstacles = set()
+    obstructions = set()
     sx, sy = 0, 0
-    boundary = 0
 
     for y, line in enumerate(sys.stdin):
-        boundary = max(boundary, y)
+        edge = y
 
         for x, char in enumerate(line.strip()):
             if char == "#":
-                obstacles.add((x, y))
+                obstructions.add((x, y))
             elif char == "^":
                 sx, sy = x, y
 
-    path, _ = find(sx, sy, obstacles, boundary)
+    path, _ = traverse(sx, sy, obstructions, edge)
+    placements = sum(
+        has_cycle
+        for _, has_cycle in (
+            traverse(sx, sy, obstructions | {(x, y)}, edge) for x, y in path
+        )
+    )
 
     print("Part 1:", len(path))
-    print("Part 2:", search(sx, sy, path, obstacles, boundary))
+    print("Part 2:", placements)
 
 
 if __name__ == "__main__":

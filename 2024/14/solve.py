10b13a53a73ed0854c82e437fb6180e0743f36b2
John Pignata <john@pignata.com>
2024-12-14 11:31:33 -0500

    2024/14, clean-up

--- 2024/14/solve.py
+++ 2024/14/solve.py
@@ -1,39 +1,33 @@
-import re
 import sys
+from collections import defaultdict
+from itertools import count
+from math import prod
+from re import findall
 
 guards = []
-maxx = maxy = seconds = 0
+maxx = maxy = 0
 
 for line in sys.stdin:
-    px, py, vx, vy = list(map(int, re.findall(r"-?\d+", line)))
+    px, py, vx, vy = list(map(int, findall(r"-?\d+", line)))
     maxx = max(maxx, px + 1)
     maxy = max(maxy, py + 1)
 
     guards.append(((px, py), (vx, vy)))
 
-while True:
-    if seconds == 100:
-        midx, midy = maxx // 2, maxy // 2
-        counts = [0] * 4
+for second in count():
+    counts = defaultdict(int)
 
-        for (px, py), _ in guards:
-            if px < midx and py < midy:
-                counts[0] += 1
-            elif px < midx and py > midy:
-                counts[1] += 1
-            elif px > midx and py < midy:
-                counts[2] += 1
-            elif px > midx and py > midy:
-                counts[3] += 1
+    for (px, py), _ in guards:
+        counts[(px < maxx // 2, py < maxy // 2)] += 1
 
-        print("Part 1:", counts[0] * counts[1] * counts[2] * counts[3])
+    if second == 100:
+        print("Part 1:", prod(counts.values()))
 
     if len(guards) == len({(x, y) for (x, y), _ in guards}):
-        print("Part 2:", seconds)
+        print("Part 2:", second)
         break
 
     guards = [
         ((((px + vx) % maxx, (py + vy) % maxy)), (vx, vy))
         for (px, py), (vx, vy) in guards
     ]
-    seconds += 1

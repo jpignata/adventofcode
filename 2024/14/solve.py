f9db2f24d5709c10635646e0113ecdc0ee72998c
John Pignata <john@pignata.com>
2024-12-14 11:49:56 -0500

    2024/14, speed up and return correct answer

--- 2024/14/solve.py
+++ 2024/14/solve.py
@@ -1,5 +1,5 @@
 import sys
-from collections import defaultdict
+from collections import Counter
 from itertools import count
 from math import prod
 from re import findall
@@ -9,18 +9,18 @@ maxx = maxy = 0
 
 for line in sys.stdin:
     px, py, vx, vy = list(map(int, findall(r"-?\d+", line)))
-    maxx = max(maxx, px + 1)
-    maxy = max(maxy, py + 1)
+    maxx, maxy = max(maxx, px + 1), max(maxy, py + 1)
 
     guards.append(((px, py), (vx, vy)))
 
 for second in count():
-    counts = defaultdict(int)
-
-    for (px, py), _ in guards:
-        counts[(px < maxx // 2, py < maxy // 2)] += 1
-
     if second == 100:
+        counts = Counter(
+            (px < midx, py < midy)
+            for (px, py), _ in guards
+            if px != (midx := maxx // 2) and py != (midy := maxy // 2)
+        )
+
         print("Part 1:", prod(counts.values()))
 
     if len(guards) == len({(x, y) for (x, y), _ in guards}):

8e1935cbbab8b1e1c80c1699b58f2e604e30eacb
John Pignata <john@pignata.com>
2025-12-06 08:00:25 -0500

    2025/06, clean-up, maybe

--- 2025/06/solve.py
+++ 2025/06/solve.py
@@ -3,44 +3,34 @@ from functools import reduce
 from operator import add, mul
 
 
-def evaluate_columns(data):
-    total = 0
+def parse_columns(lines):
+    problems = [list(line.strip().split()) for line in lines]
+
+    return [
+        (operators[problem[-1]], [int(value) for value in problem[:-1]])
+        for problem in zip(*problems)
+    ]
+
+
+def parse_digits(lines):
     problems = []
-    operators = {"+": add, "*": mul}
+    values = []
 
-    for line in data:
-        problems.append([])
-
-        for token in line.strip().split():
-            if token in operators:
-                problems[-1].append(operators[token])
-            else:
-                problems[-1].append(int(token))
-
-    for problem in list(zip(*problems)):
-        total += reduce(problem[-1], problem[:-1])
-
-    return total
-
-
-def evaluate_digits(data):
-    total = 0
-    stack = []
-    operators = {"+": add, "*": mul}
-
-    for column in list(zip(*[line[::-1] for line in data]))[1:]:
+    for column in list(zip(*[line[::-1] for line in lines]))[1:]:
         if not "".join(column).strip():
             continue
 
-        stack.append(int("".join(column[:-1])))
+        values.append(int("".join(column[:-1])))
 
         if column[-1] in operators:
-            total += reduce(operators[column[-1]], stack)
-            stack = []
+            problems.append((operators[column[-1]], values))
+            values = []
 
-    return total
+    return problems
 
 
-lines = sys.stdin.readlines()
-print("Part 1:", evaluate_columns(lines))
-print("Part 2:", evaluate_digits(lines))
+data = sys.stdin.readlines()
+operators = {"+": add, "*": mul}
+
+print("Part 1:", sum(reduce(op, values) for op, values in parse_columns(data)))
+print("Part 2:", sum(reduce(op, values) for op, values in parse_digits(data)))
